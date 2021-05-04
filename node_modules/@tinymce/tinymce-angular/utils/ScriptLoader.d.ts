4b00262a0c39cdaeb08001479e4e91ecb3942b34
Neimhin <nrobinso@tcd.ie>
2021-05-04 16:51:21 +0100

    delete node_modules from git

--- node_modules/@tinymce/tinymce-angular/utils/ScriptLoader.d.ts
+++
@@ -1,8 +0,0 @@
-export declare type callbackFn = () => void;
-export interface IStateObj {
-    listeners: callbackFn[];
-    scriptId: string;
-    scriptLoaded: boolean;
-}
-export declare const create: () => IStateObj;
-export declare const load: (state: IStateObj, doc: Document, url: string, callback: callbackFn) => void;
--- node_modules/angular-text-input-highlight/index.js
+++
@@ -1,2 +0,0 @@
-export * from './text-input-highlight.module';
-//# sourceMappingURL=index.js.map
