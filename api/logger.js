7c92fb73966deb70570a6c999e620b21cfe07e1f
Neimhin <nrobinso@tcd.ie>
2021-05-10 18:33:12 +0100

    replaced console.log with logger.info for MongoDB connection message

--- api/logger.js
+++ api/logger.js
@@ -1,19 +1,33 @@
 
 
-const winston = require('winston')
-const fs = require('fs')
-const path = require('path')
+const winston = require('winston');
+const fs = require('fs');
+const path = require('path');
 
 
 const errorFile = path.join(__dirname, 'logs/error.log')
 const combinedFile = path.join(__dirname, 'logs/combined.log')
 
+
+const myFormat = winston.format.printf( ({ level, message, timestamp, ...metadata}) => {
+  let msg = `${timestamp} [${level}] : ${message} `
+  if(metadata) {
+    msg += JSON.stringify(metadata)
+  }
+  return msg
+});
+
 // Create our logger object
 const logger = winston.createLogger({
+  format: 
+      winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        myFormat
+      ),
   transports: [
     // This transport lets us log to the console
-    new winston.transports.Console({
-      winston.format),
+    new winston.transports.Console(),
     // This transport logs to the error file
     new winston.transports.File({ filename: errorFile, level: 'error' }),
     // This transport should be were everything gets sent
@@ -22,6 +36,9 @@ const logger = winston.createLogger({
 });
 
 
+module.exports = logger
+
+/*
 const express = require('express');
 const app = express()
 const port = 3000;
@@ -40,3 +57,4 @@ const handler = (func) => (req, res) => {
 app.get('/success', handler((req, res) => { res.send('Yay!'); }))
 app.get('/error', handler((req, res) => { throw new Error('Doh!');}))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+*/
