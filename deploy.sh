67619323088cf6d103a535949d34c33e1b3aa9ce
Neimhin <nrobinso@tcd.ie>
2021-05-21 16:51:04 +0100

    update deploy.sh and include node namespace in tsconfig

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,13 @@
 
 # Pull latest code from git repo
 git pull
+git checkout master
 
 # Clean install node dependencies
-npm ci
-npm ci --prefix ./api
+rm -r node_modules
+npm install
+rm -r api/node_modules
+npm install --prefix ./api
 
 # String replace backend address in abairconfig.json
 sed -i 's/http:\/\/localhost:4000\//https:\/\/www.abair.tcd.ie\/anscealaibackend\//' src/abairconfig.json
