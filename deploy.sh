3ead2b9eb64472309815fe5231399bd8bdacaaa8
Neimhin <nrobinso@tcd.ie>
2021-06-08 23:33:52 +0100

    trying to make the deploy script a bit less risky

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
 # Pull latest code from git repo
-git pull
-git checkout master
+# Maybe we should hold off on pulling automatically until we have a good test suite
+#git pull
+#git checkout master
 
 # Clean install node dependencies
-rm -r ngapp/node_modules
 npm install --prefix ./ngapp
-rm -r api/node_modules
 npm install --prefix ./api
 
-# String replace backend address in abairconfig.json
-sed -i 's/http:\/\/localhost:4000\//https:\/\/www.abair.tcd.ie\/anscealaibackend\//' src/abairconfig.json
+# String replace backend address in abairconfig.json   (Now done in ngapp's package.json)
+#sed -i 's/http:\/\/localhost:4000\//https:\/\/www.abair.tcd.ie\/anscealaibackend\//' ngapp/src/abairconfig.json
 
-# Build to dist/ directory with /scealai/ as base href
-npm --prefix ngapp run build
+# Build to ngapp/unpublished_dist/ directory with /scealai/ as base href
+npm --prefix ngapp run build_prod
+
+if [ $? != 0 ] ; then
+  exit 1
+fi
+CURRENT_DATE=`date +"%d_%m_%Y_%T"`
+
+# Create a backup of the live site with the current date in the name
+BACKUP_DIR_NAME=./dist/backup_${CURRENT_DATE}
+echo "Moving ./dist/an-scealai to ${BACKUP_DIR_NAME}" 
+mv ./dist/an-scealai ${BACKUP_DIR_NAME}
+
+echo "Copying ./ngapp/unpublished_dist to ./dist/an-scealai"
+cp -r ./ngapp/unpublished_dist ./dist/an-scealai
