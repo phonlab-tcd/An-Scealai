54fdec9f1a30c5366ea3d9bbdfae6369f25ac714
Neimhin <nrobinso@tcd.ie>
2021-06-09 13:03:44 +0100

    some comments in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,24 @@
 #git pull
 #git checkout master
 
-# Clean install node dependencies
+# install node dependencies
+# Not sure if we should delete node_modules before installing.
 npm install --prefix ./ngapp
 npm install --prefix ./api
 
-# String replace backend address in abairconfig.json   (Now done in ngapp's package.json)
+# String replace backend address in abairconfig.json   
+# (Now done in ngapp's package.json)
 #sed -i 's/http:\/\/localhost:4000\//https:\/\/www.abair.tcd.ie\/anscealaibackend\//' ngapp/src/abairconfig.json
 
 # Build to ngapp/unpublished_dist/ directory with /scealai/ as base href
 npm --prefix ngapp run build_prod
 
 if [ $? != 0 ] ; then
+  echo "I think the build failed. Exiting deployment."
   exit 1
 fi
+
+# Use current date in name of backup folder
 CURRENT_DATE=`date +"%d_%m_%Y_%T"`
 
 # Create a backup of the live site with the current date in the name
