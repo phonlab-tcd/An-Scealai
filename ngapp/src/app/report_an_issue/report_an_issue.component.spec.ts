b4e705519c7ce582fe0c248325f935aa18369e3f
Neimhin <nrobinso@tcd.ie>
2021-06-13 16:10:26 +0100

    got testing running for report_an_issue page

--- ngapp/src/app/report_an_issue/report_an_issue.component.spec.ts
+++ ngapp/src/app/report_an_issue/report_an_issue.component.spec.ts
@@ -1,25 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { SponsorsComponent } from './sponsors.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReportAnIssueComponent } from './report_an_issue.component';
 
 describe('SponsorsComponent', () => {
-  let component: SponsorsComponent;
-  let fixture: ComponentFixture<SponsorsComponent>;
+  let component: ReportAnIssueComponent;
+  let fixture: ComponentFixture<ReportAnIssueComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SponsorsComponent ]
+      schemas: [ NO_ERRORS_SCHEMA ],
+      imports: [ RouterTestingModule, HttpClientTestingModule],
+      declarations: [ ReportAnIssueComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SponsorsComponent);
+    fixture = TestBed.createComponent(ReportAnIssueComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have access to the translation service', () => {
+    expect(component.ts).toBeTruthy();
   });
 });
