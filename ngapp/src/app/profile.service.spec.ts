65babb077935502ca97194e47b2a7e2e1a0f0d58
Maddie <maddie.comtois@gmail.com>
2021-06-24 15:22:37 +0200

    TestBed.get -> TestBed.inject

--- ngapp/src/app/profile.service.spec.ts
+++ ngapp/src/app/profile.service.spec.ts
@@ -9,7 +9,7 @@ describe('ProfileService', () => {
   }));
 
   it('should be created', () => {
-    const service: ProfileService = TestBed.get(ProfileService);
+    const service: ProfileService = TestBed.inject(ProfileService);
     expect(service).toBeTruthy();
   });
 });
