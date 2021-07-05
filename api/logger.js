d5ac7ffd08bb8aa59acbcd2406a92336b2eecf2c
Neimhin <nrobinso@tcd.ie>
2021-07-05 16:57:00 +0100

    can't quite remember what's in this commit

--- api/logger.js
+++ api/logger.js
@@ -8,7 +8,7 @@ var logger ={ error: console.error };
 
 // TODO: I'm not sure if this line should be included
 process.on('uncaughtException', err => {
-  logger.error( "UNCAUGHT EXCEPTION" );
+  logger.error({ title: "UNCAUGHT EXCEPTION", error: err } );
   console.dir(err);
 });
 
@@ -16,16 +16,40 @@ const errorFile = path.join(__dirname, 'logs/error.log');
 const combinedFile = path.join(__dirname, 'logs/combined.log');
 const uncaughtExceptionsFile = path.join(__dirname, 'logs/uncaughtExceptions.log');
 
+const enumerateErrorFormat = winston.format(info => {
+  if (info.message instanceof Error) {
+    info.message = Object.assign({
+      message: info.message.message,
+      stack: info.message.stack,
+    }, info.message);
+  }
+  if (info instanceof Error) {
+    return Object.assign({
+      message: info.message,
+      stack: info.stack,
+    }, info);
+  }
+  return info;
+});
+
 const consoleFormat = winston.format.printf(
   ({ level, message, timestamp, ...metadata}) => {
     // TODO log the rest parameters in metadata
     let string_message = JSON.stringify(message);
+    let metaDataMsg = '';
+    for(let d in metadata){
+      metaDataMsg += JSON.strinfiy(d) + ' : ';
+    }
+    metaDataMsg += 'END';
     let msg = `${timestamp} [${level}] : ${string_message}`;
     return msg;
   })
 
 // Create our logger object
 logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    enumerateErrorFormat()),
   transports: [
     // This transport lets us log to the console
     new winston.transports.Console({
