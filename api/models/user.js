031e408012a37fc661a557a5f9ac106416bfb909
Neimhin <nrobinso@tcd.ie>
2021-07-06 16:07:57 +0100

    styling

--- api/models/user.js
+++ api/models/user.js
@@ -23,54 +23,65 @@ const resetPasswordSchema = new mongoose.Schema({
 });
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        index: true,
-        unique: true,
+  username: {
+    type: String,
+    required: true,
+    index: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    default: null,
+  },
+  role: {
+    type: String,
+    enum: ['ADMIN', 'TEACHER', 'STUDENT'],
+    default: 'STUDENT',
+  },
+  language: {
+    type: String,
+    enum: ['ga', 'en'],
+    default: 'en',
+  },
+  hash: String,
+  salt: String,
+  status: {
+    type: String,
+    enum: ['Pending', 'Active'],
+    default: 'Pending',
+  },
+  verification: {
+    type: verificationSchema,
+    unique: true,
+    default: {
+      code: null,
+      date: null,
     },
-    email: {
-      type: String,
-      default: null,
-    },
-    role: {
-        type: String,
-        enum: ['ADMIN', 'TEACHER', 'STUDENT'],
-        default: 'STUDENT'
-    },
-    language: {
-        type: String,
-        enum: ['ga', 'en'],
-        default: 'en'
-    },
-    hash: String,
-    salt: String,
-    status: {
-      type: String,
-      enum: ['Pending','Active'],
-      default: 'Pending',
-    },
-    verification: {
-      type: verificationSchema,
-      unique: true,
-      default: {
-        code: null,
-        date: null,
-      },
-    },
-    resetPassword: {
-      type: resetPasswordSchema,
-      unique: true,
-    }
+  },
+  resetPassword: {
+    type: resetPasswordSchema,
+    unique: true,
+  },
 });
 
-userSchema.methods.setPassword = function(password){
-    this.salt = crypto.randomBytes(16).toString('hex');
-    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+userSchema.methods.setPassword = function(password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(
+      password,
+      this.salt,
+      1000,
+      64,
+      'sha512').toString('hex');
 };
 
 userSchema.methods.validPassword = function(password) {
-    return this.hash === crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+  return this.hash === crypto
+      .pbkdf2Sync(
+          password,
+          this.salt,
+          1000,
+          64,
+          'sha512').toString('hex');
 };
 
 userSchema.methods.validStatus = function() {
@@ -79,46 +90,67 @@ userSchema.methods.validStatus = function() {
   }
   const allowableStatus = /^(Active|Pending)$/;
   return this.status.match(allowableStatus);
-}
-
-userSchema.methods.generateJwt = function() {
-    var expiry = new Date();
-    expiry.setDate(expiry.getDate() + 7);
-
-    return jwt.sign({
-        _id: this._id,
-        username: this.username,
-        role: this.role,
-        language: this.language,
-        exp: parseInt(expiry.getTime() / 1000 /* convert milliseconds to seconds */),
-    }, "sonJJxVqRC"); // 5ecret
 };
 
-userSchema.methods.generateNewPassword = function () {
+userSchema.methods.generateJwt = function() {
+  const expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    username: this.username,
+    role: this.role,
+    language: this.language,
+    exp: parseInt(
+        expiry.getTime() / 1000 /* convert milliseconds to seconds */),
+  }, 'sonJJxVqRC'); // 5ecret
+};
+
+userSchema.methods.generateNewPassword = function() {
   return generate_password.generate({
     length: 10,
-    numbers: true
+    numbers: true,
   });
-}
+};
 
-userSchema.methods.generateResetPasswordLink = function (baseurl) {
+// NOTE This function does not save the details.
+// They must be saved with <document>.save();
+userSchema.methods.generateResetPasswordLink = function(baseurl) {
   if ( ! this.resetPassword ) {
     this.resetPassword = {};
   }
 
-  this.resetPassword.code = jwt.sign({username: this.username, email: this.email}, 'sonJJxVqRC');
-  this.resetPassword.date = new Date();
-  return `${baseurl}user/generateNewPassword?username=${this.username}&email=${this.email}&code=${this.resetPassword.code}`;
-}
+  this.resetPassword.code = jwt.sign({
+    username: this.username,
+    email: this.email,
+  }, 'sonJJxVqRC');
 
-userSchema.methods.generateActivationLink = function (baseurl) {
+  this.resetPassword.date = new Date();
+
+  return `${baseurl}user/generateNewPassword` +
+    `?username=${this.username}` +
+    `&email=${this.email}` +
+    `&code=${this.resetPassword.code}`;
+};
+
+// NOTE This function does not save the details.
+// They must be saved with <document>.save();
+userSchema.methods.generateActivationLink = function(baseurl) {
   // Make sure this.verification exists
   if ( ! this.verification ) {
     this.verification = {};
   }
-  this.verification.code = jwt.sign({username: this.username, email: this.email},'sonJJxVqRC');
+  this.verification.code = jwt.sign({
+    username: this.username,
+    email: this.email,
+  }, 'sonJJxVqRC');
+
   this.verification.date = new Date();
-  return `${baseurl}user/verify?username=${this.username}&email=${this.email}&verificationCode=${this.verification.code}`;
-}
+
+  return `${baseurl}user/verify` +
+    `?username=${this.username}` +
+    `&email=${this.email}` +
+    `&verificationCode=${this.verification.code}`;
+};
 
 module.exports = mongoose.model('User', userSchema);
