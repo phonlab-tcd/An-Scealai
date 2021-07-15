1ff4be721f762edd9aae6e123a077e5a27243e9f
Neimhin <nrobinso@tcd.ie>
2021-07-15 12:43:39 +0100

    update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -3,4 +3,4 @@ rm -rf api/node_modules
 
 # redirect stdout and stderr to installAPI.log
 npm i --prefix api >> installAPI.log 2>&1 &
-npm i --prefix ngapp >> installNG.log 2>&1 &
+npm i --prefix ngapp | tee installNG.log 2>&1 &
