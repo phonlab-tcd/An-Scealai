0c260c08149f9a2d07177eed63fdc38f43b828b9
Neimhin <nrobinso@tcd.ie>
2021-08-10 12:00:20 +0100

    Importing HttpClientTestingModule and RouterTestingModule in GrammarChecker spec. These are required since the component now handles its backend requests by itself.
    It may be possible to remove the RouterTestingModule as a dependency of
    this component

--- ngapp/src/app/student-components/grammar-checker/grammar-checker.component.spec.ts
+++ ngapp/src/app/student-components/grammar-checker/grammar-checker.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { GrammarCheckerComponent } from './grammar-checker.component';
+import {Router} from '@angular/router';
 
 describe('GrammarCheckerComponent', () => {
   let component: GrammarCheckerComponent;
@@ -8,7 +10,9 @@ describe('GrammarCheckerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GrammarCheckerComponent ]
+      declarations: [ GrammarCheckerComponent ],
+      imports: [ HttpClientTestingModule, RouterTestingModule ],
+      // providers: [],
     })
     .compileComponents();
   });
