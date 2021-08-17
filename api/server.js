89ce2f985f1627944df16abf7e2fcd8f2ff0b132
Neimhin <nrobinso@tcd.ie>
2021-08-17 13:57:32 +0100

    dbUrl util

--- api/server.js
+++ api/server.js
@@ -27,9 +27,7 @@ mongoose.Promise = global.Promise;
 mongoose.set('useFindAndModify', false);
 
 // use the URL for the test DB if it has been set, otherwise use the normal DB.
-const dbURL =
-  process.env.TEST_MONGO_URL ||
-  (config.DB_AUTH_DETAILS + config.DB_URL_PREFIX + config.DB_NAME);
+const dbURL = require('./utils/dbUrl');
 
 mongoose.connect(dbURL, {
   useNewUrlParser: true,
