959cb5b8c6312e24dc19d105d984a2479691f79c
Neimhin <nrobinso@tcd.ie>
2021-08-17 13:49:50 +0100

    rename DB.template.js

---
+++ api/DB.template.js
@@ -0,0 +1,10 @@
+module.exports = {
+  DB_URL_PREFIX: 'mongodb://',
+  DB_AUTH_DETAILS: '',
+  DB_HOSTNAME: 'localhost:27017/',
+  DB_NAME: 'an-scealai',
+  // if not using authentication use empty string for auth details:
+  // DB_AUTH_DETAILS: '',
+  // OR
+  // DB_AUTH_DETAILS: '<username>:<password>@',
+};
