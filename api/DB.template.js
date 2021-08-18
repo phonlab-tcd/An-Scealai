07eda1435ce02753be6fa2b64c736e151e87061d
Neimhin <nrobinso@tcd.ie>
2021-08-18 11:32:30 +0100

    update DB.template.js

---
+++ api/DB.template.js
@@ -0,0 +1,8 @@
+module.exports = {
+  DB_NAME: 'an-scealai',
+  DB_URL_PREFIX: 'mongodb://',
+  DB_AUTH_DETAILS: '',
+  DB_HOSTNAME: 'localhost:27017/',
+  // if not using authentication use empty string for auth details:
+  // DB_AUTH_DETAILS: '<username>:<password>@',
+};
