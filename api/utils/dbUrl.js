e635763a8de85ed28a825297dc11c73cf05ddaec
Neimhin <nrobinso@tcd.ie>
2021-09-10 21:26:59 +0100

    configurable dbUrl

--- api/utils/dbUrl.js
+++ api/utils/dbUrl.js
@@ -2,4 +2,7 @@ const config = require('../DB');
 // use the URL for the test DB if it has been set, otherwise use the normal DB.
 module.exports =
   process.env.TEST_MONGO_URL ||
-  config.DB;
+  (process.env.DB_URL_PREFIX   || config.DB_URL_PREFIX   ) +
+  (process.env.DB_AUTH_DETAILS || config.DB_AUTH_DETAILS ) +
+  (process.env.DB_HOSTNAME     || config.DB_HOSTNAME     ) +
+  (process.env.DB              || config.DB              );
