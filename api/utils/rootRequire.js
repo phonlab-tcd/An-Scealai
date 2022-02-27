63b95241013f1016952c10725a2044246cdb3884
Neimhin <nrobinso@tcd.ie>
2022-02-27 20:58:26 +0000

    change name of rootRequire util

--- api/utils/rootRequire.js
+++
@@ -1,8 +0,0 @@
-// Require a file relative to 'api/endpoints_functions'
-module.exports = (str) => {
-  return require.main.require('./endpoints_functions' + str);
-}
-
-// e.g.
-// const root_require = require('../utils/rootRequire');
-// root_require('/story/create');
