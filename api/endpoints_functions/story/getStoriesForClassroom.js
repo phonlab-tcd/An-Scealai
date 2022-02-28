29c59e7ea1ba899836c9f315bf83688fe876b373
Neimhin <nrobinso@tcd.ie>
2022-02-28 12:13:48 +0000

    minor formatting

--- api/endpoints_functions/story/getStoriesForClassroom.js
+++ api/endpoints_functions/story/getStoriesForClassroom.js
@@ -1,4 +1,5 @@
 const Story = require.main.require('./models/story');
+
 module.exports = (req, res, next) => {
   Story.find({author: req.params.author, date: {$gte: req.params.date}},
     (err, stories) => {
