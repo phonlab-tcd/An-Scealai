133d06afdba8e877ecf8a1e8a8e5088fbad163fe
Neimhin <nrobinso@tcd.ie>
2022-02-28 12:43:35 +0000

    create module for cur síos (picture description game) and image component

---
+++ ngapp/src/cur-síos/cur-síos.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ImageComponent } from './component/image/image.component';
+
+
+@NgModule({
+  declarations: [
+    ImageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CurSíosModule { }
