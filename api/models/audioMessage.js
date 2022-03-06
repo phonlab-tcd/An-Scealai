19c221f46b4d15810e1ceb981f4b9d5e1853c2da
Neimhin <nrobinso@tcd.ie>
2022-03-06 23:37:11 +0000

    remove dir from audioMessage models
    - derive path from other fields
    - api/audioMessage/<ownerId>/<messageId>.<mimetype>

--- api/models/audioMessage.js
+++ api/models/audioMessage.js
@@ -6,7 +6,6 @@ const AudioMessage = new Schema({
     ownerId: mongoose.ObjectId,
     public: Boolean,
     reciptients: [mongoose.ObjectId],
-    dir: String,
     mimetype: String,
 }, {
     collection: 'description.game.audio.message'
@@ -15,18 +14,18 @@ const AudioMessage = new Schema({
 AudioMessage.methods.path = function () {
   if(!this)
     throw new Error('document does not exist yet');
-  if(!this.dir)
+  if(!this.ownerId)
     throw new Error('dir must be defined to derive path');
   if(!this._id)
     throw new Error('_id must be defined to derive path');
   if(!this.mimetype)
     throw new Error('mimetype must be defined to derive path');
   return path.join(
-    this.dir,
-    ''.concat(
-      this._id,
-      '.',
-      this.mimetype.split('/')[1]));
+    __dirname,
+    '..',
+    'audioMessages',
+    this.ownerId.toString(),
+    ''.concat(this._id, '.', this.mimetype.split('/')[1]));
 };
 
 module.exports = mongoose.model('AudioMessage', AudioMessage);
