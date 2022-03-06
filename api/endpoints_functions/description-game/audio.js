4d0b7566dbfb6445efc31562b59f475efd28fcfd
Neimhin <nrobinso@tcd.ie>
2022-03-06 21:00:34 +0000

    move /description-gam/audio func to own file

---
+++ api/endpoints_functions/description-game/audio.js
@@ -0,0 +1,51 @@
+const app = require('../../server');
+const fs = require('fs').promises;
+const path = require('path');
+const AudioMessage = require('../../models/audioMessage');
+
+function validAudioForms(files)  {
+  if(!files) return false;
+  for(const f of files) if(!f.buffer || !f.mimetype) return false;
+  return true;
+}
+
+async function ensureDirExists(dir) {
+  return fs.mkdir(dir).catch(e=>{
+    if(e.code==='EEXIST')return
+    else throw e
+  })
+}
+
+const apiRootDir = path.join(__dirname,'..','..');
+
+module.exports = async (req,res,next) => {
+  if(!validAudioForms(req.files)) {
+    console.log(req.files);
+    return next(new Error('req.files is not valid'));
+  }
+  app.logger.info({
+    route: '/description-game/audio',
+    user: req.user,
+    body: req.body,
+    numFiles: req.files.length,
+  });
+  const dir = path.join(
+    apiRootDir,
+    'audioMessages',
+    req.user._id);
+  const [_, audioMessages] = await Promise.all([
+    ensureDirExists(dir),
+    AudioMessage.create(req.files.map(file => {
+      return {
+        ownerId: req.user._id,
+        public: true,
+        dir: dir.toString(),
+        mimetype: file.mimetype,
+      }
+    })),
+  ]);
+  await audioMessages.map(async (am,i)=>{
+    await fs.writeFile(am.path(), req.files[i].buffer)
+  });
+  res.json(audioMessages);
+}
--- api/routes/description-game.route.js
+++ api/routes/description-game.route.js
@@ -2,49 +2,25 @@ const app = require('../server');
 const descriptionGameRoutes = require('express').Router();
 const jwtAuthMw = require('../utils/jwtAuthMw');
 const multer = require('multer')();
-const Promise = require('bluebird');
 const path = require('path');
-const fs = require('fs').promises;
-const AudioMessage = require('../models/audioMessage');
 
+function ep(_path) {
+  _path = path.join('..','endpoints_functions','description-game',_path);
+  return (req,res,next) => {
+    try {
+      require(_path)(req,res,next)
+    } catch(e) {
+      next(e);
+    }
+  }
+}
 
 descriptionGameRoutes
   .route('/audio')
   .post(
     jwtAuthMw,
     multer.any(),
-    async (req,res,next) => {
-      app.logger.info({
-        route: '/description-game/audio',
-        user: req.user,
-        body: req.body,
-        numFiles: req.files.length,
-      });
-      const dir = path.join(
-        __dirname,
-        '..',
-        'audioMessages',
-        req.user._id);
-      const [_, audioMessages] = await Promise.all([
-        fs.mkdir(dir).catch(e=>{
-          if(e.code === 'EEXIST')return
-          else throw e
-        }),
-        AudioMessage.create(req.files.map(file => {
-          return {
-            public: true,
-            ownerId: req.user._id,
-            dir: dir.toString(),
-            mimetype: file.mimetype,
-          }
-        })),
-      ]);
-      await audioMessages.map(async (am,i)=>{
-        console.log(am);
-        await fs.writeFile(am.path(), req.files[i].buffer)
-      });;
-      res.sendStatus(200);
-    });
+    ep('audio'));
 
 module.exports = descriptionGameRoutes;
 
