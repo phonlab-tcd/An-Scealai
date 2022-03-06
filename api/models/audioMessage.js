27208a5b943985e5744282910e703a81669389fb
Neimhin <nrobinso@tcd.ie>
2022-03-06 19:46:11 +0000

    audioMessage Schema and description game post audio endpoint

---
+++ api/models/audioMessage.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const path = require('path');
+
+const AudioMessage = new Schema({
+    ownerId: mongoose.ObjectId,
+    public: Boolean,
+    reciptients: [mongoose.ObjectId],
+    dir: String,
+    mimetype: String,
+}, {
+    collection: 'description.game.audio.message'
+});
+
+AudioMessage.methods.path = function () {
+  if(!this)
+    throw new Error('document does not exist yet');
+  if(!this.dir)
+    throw new Error('dir must be defined to derive path');
+  if(!this._id)
+    throw new Error('_id must be defined to derive path');
+  if(!this.mimetype)
+    throw new Error('mimetype must be defined to derive path');
+  return path.join(
+    this.dir,
+    ''.concat(
+      this._id,
+      '.',
+      this.mimetype.split('/')[1]));
+};
+
+module.exports = mongoose.model('AudioMessage', AudioMessage);
--- api/routes/description-game.route.js
+++ api/routes/description-game.route.js
@@ -2,20 +2,47 @@ const app = require('../server');
 const descriptionGameRoutes = require('express').Router();
 const jwtAuthMw = require('../utils/jwtAuthMw');
 const multer = require('multer')();
-const fs = require('fs');
+const Promise = require('bluebird');
+const path = require('path');
+const fs = require('fs').promises;
+const AudioMessage = require('../models/audioMessage');
+
 
 descriptionGameRoutes
   .route('/audio')
   .post(
     jwtAuthMw,
     multer.any(),
-    (req,res,next) => {
-      const buffer = req.files[0].buffer;
-      req.files[0].buffer = undefined;
-      app.logger.info(
-        {user: req.user,
-          body: req.body,
-          file0: req.files[0]});
+    async (req,res,next) => {
+      app.logger.info({
+        route: '/description-game/audio',
+        user: req.user,
+        body: req.body,
+        numFiles: req.files.length,
+      });
+      const dir = path.join(
+        __dirname,
+        '..',
+        'audioMessages',
+        req.user._id);
+      const [_, audioMessages] = await Promise.all([
+        fs.mkdir(dir).catch(e=>{
+          if(e.code === 'EEXIST')return
+          else throw e
+        }),
+        AudioMessage.create(req.files.map(file => {
+          return {
+            public: true,
+            ownerId: req.user._id,
+            dir: dir.toString(),
+            mimetype: file.mimetype,
+          }
+        })),
+      ]);
+      await audioMessages.map(async (am,i)=>{
+        console.log(am);
+        await fs.writeFile(am.path(), req.files[i].buffer)
+      });;
       res.sendStatus(200);
     });
 
