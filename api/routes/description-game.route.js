71a679def055b996989a74e3480dad242661fbbd
Neimhin <nrobinso@tcd.ie>
2022-03-07 00:00:34 +0000

    move handlers to their own files

--- api/routes/description-game.route.js
+++ api/routes/description-game.route.js
@@ -3,12 +3,17 @@ const descriptionGameRoutes = require('express').Router();
 const jwtAuthMw = require('../utils/jwtAuthMw');
 const multer = require('multer')();
 const path = require('path');
+const mongoose = require('mongoose');
+const fs = require('fs').promises;
 
-function ep(_path) {
+function ep(_path,method) {
   _path = path.join('..','endpoints_functions','description-game',_path);
   return (req,res,next) => {
     try {
-      require(_path)(req,res,next)
+      const module = require(_path);
+      if(method)
+          return module[method](req,res,next);
+      return module(req,res,next);
     } catch(e) {
       next(e);
     }
@@ -16,11 +21,15 @@ function ep(_path) {
 }
 
 descriptionGameRoutes
-  .route('/audio')
-  .post(
+  .post('/audio',
     jwtAuthMw,
     multer.any(),
-    ep('audio'));
+    ep('audio','post'));
+
+descriptionGameRoutes
+  .get('/audio/:id',
+    jwtAuthMw,
+    ep('audio','get'));
 
 module.exports = descriptionGameRoutes;
 
