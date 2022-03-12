a6e13353c4752fec2f7fa25f1e04b382e50cbaa1
Neimhin <nrobinso@tcd.ie>
2022-03-12 17:19:56 +0000

    /description-game/next/describe endpoint

---
+++ api/endpoints_functions/description-game/next/describe.js
@@ -0,0 +1,49 @@
+const DescribeGame = require('../../../models/description-game/describe-game');
+const ObjectId = require('mongoose').Types.ObjectId;
+const fs = require('fs').promises;
+const path = require('path');
+const imageDir = require('../../../utils/imageDir');
+const { API400Error } = require('../../../utils/APIError');
+
+async function validParams(params, res, next) {
+  params.public = params.public === false ? false : true;
+  console.log(imageDir);
+  if(!params.audioMessages) {
+    res.status(400);
+    next(new API400Error('audioMessages not defined'));
+    return false;
+  }
+  for(const param of ['reciptients','audioMessages']) {
+    try { params[param] = params[param].map(v=>ObjectId(v)); }
+    catch (e) { next(e); return false; }
+  }
+  try { 
+    const promises = [
+      fs.stat(path.join(imageDir,''+params.imagePath)),
+    ];
+    await Promise.all(promises); }
+  catch (e) { next(e); return false; }
+  return true;
+};
+
+module.exports = {};
+module.exports.get = async (req,res,next) => {
+  const currentGame = await DescribeGame.findOne(
+    {ownerId: ObjectId(req.user._id), 'time.finished': null });
+  if(currentGame) {
+    return res.json(currentGame);
+  };
+  const newGame = await DescribeGame.create({
+    ownerId: req.user._id,
+    public: false,
+    published: false,
+    recipients: [],
+    audioMessages: [],
+    imagePath: req.params.imagePath,
+    time: {
+      started: new Date(),
+      finished: null,
+    },
+  });
+  res.json(newGame);
+};
