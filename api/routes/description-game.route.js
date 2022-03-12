3a76b7596cad81a179de17901913a7eec2c9b724
Neimhin <nrobinso@tcd.ie>
2022-03-12 22:30:03 +0000

    global jwtAuthMw for /description-game add /next/describe /next/interpret

--- api/routes/description-game.route.js
+++ api/routes/description-game.route.js
@@ -3,11 +3,12 @@ const descriptionGameRoutes = require('express').Router();
 const jwtAuthMw = require('../utils/jwtAuthMw');
 const multer = require('multer')();
 const path = require('path');
+const User = require('../models/user');
 const mongoose = require('mongoose');
 const fs = require('fs').promises;
 
 function ep(_path,method) {
-  _path = path.join('..','endpoints_functions','description-game',_path);
+  _path = path.join('..','endpoints_functions','description-game',..._path);
   return (req,res,next) => {
     try {
       const module = require(_path);
@@ -19,21 +20,37 @@ function ep(_path,method) {
   }
 }
 
+if(process.env.NO_AUTH != 1)
+  descriptionGameRoutes.use(jwtAuthMw);
+else {
+  console.log('NO AUTH for /description-game');
+  const User = require('../models/user');
+  descriptionGameRoutes.use(async (req,res,next)=>{
+    const re = /n[a-z]+/i;
+    req.user = await User.findOne({username: {$regex: re}});
+    next();
+  });
+}
+
 descriptionGameRoutes
   .post('/audio',
-    jwtAuthMw,
     multer.any(),
-    ep('audio','post'));
+    ep(['audio'],'post'));
 
 descriptionGameRoutes
   .get('/audio/:id',
-    jwtAuthMw,
-    ep('audio','get'));
+    ep(['audio'],'get'));
 
 descriptionGameRoutes
   .get('/allAudio',
-    jwtAuthMw,
-    ep('allAudio','get'));
+    ep(['allAudio'],'get'));
+
+descriptionGameRoutes
+  .get('/next/describe',
+    ep(['next','describe'],'get'));
+
+descriptionGameRoutes
+  .get('/next/interpret',
+    ep(['next','interpret'],'get'));
 
 module.exports = descriptionGameRoutes;
-
