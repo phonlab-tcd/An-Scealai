3ea5ed2cc37289e3edeef50beb6df13dde81d77d
Neimhin <nrobinso@tcd.ie>
2022-03-25 19:40:55 +0000

    select an image

--- ngapp/src/description-game/component/decide/decide.component.ts
+++ ngapp/src/description-game/component/decide/decide.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { ChangeDetectorRef, Component, HostListener, OnInit } from '@angular/core';
+import config from 'src/abairconfig.json';
 @Component({
   selector: 'description-game-decide',
   templateUrl: './decide.component.html',
   styleUrls: ['./decide.component.css']
 })
 export class DecideComponent implements OnInit {
+  possibleImages = ['/dog/001.png','/dog/002.png','/dog/001.png','/dog/002.png','/dog/001.png','/dog/002.png'].map(u => config.baseurl + 'image' + u);
 
   constructor() { }
 
