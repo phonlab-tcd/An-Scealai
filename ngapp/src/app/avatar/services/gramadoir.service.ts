3c2ea4f19a37f5b8b9583b74f104eecba2553eb8
JohnSloan8 <johnsloan1984@gmail.com>
2022-03-31 13:31:45 +0100

    making gramadoir API call from within this service

--- ngapp/src/app/avatar/services/gramadoir.service.ts
+++ ngapp/src/app/avatar/services/gramadoir.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class GramadoirService {
-  private apiUrl = "https://scealai-dashboard-server.herokuapp.com/api/v1/getIrishGramadoirCheck"
+  private apiUrl = "https://www.abair.ie/cgi-bin/api-gramadoir-1.0.pl"
   //private apiUrl = "http://localhost:5000/api/v1/getIrishGramadoirCheck"
   constructor(private http:HttpClient) { }
 
   getGramadoir = text => {
-    return this.http.post<any>(this.apiUrl, { "text": text })
+    let httpParams = new HttpParams()
+      .append("teacs", text)
+      .append("teanga", "ga")
+      
+    let headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    
+    return this.http.post<any>(this.apiUrl, httpParams, { headers })
   }
 
 }
