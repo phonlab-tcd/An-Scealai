8ccd20e0be677ceb7bddb96572fa3ff3c5f9e693
JohnSloan8 <johnsloan1984@gmail.com>
2022-03-31 13:32:06 +0100

    making tts API call from within this service

--- ngapp/src/app/avatar/services/tts.service.ts
+++ ngapp/src/app/avatar/services/tts.service.ts
@@ -8,11 +8,33 @@ import { sentences } from '../data/sentences'
 })
 
 export class TtsService {
-  private apiUrl = "https://scealai-dashboard-server.herokuapp.com/api/v1/getIrishSynthesis"
+  private apiUrl = "https://abair.ie/api2/synthesise"
+  //private apiUrl = "https://scealai-dashboard-server.herokuapp.com/api/v1/getIrishSynthesis"
   //private apiUrl = "http://localhost:5000/api/v1/getIrishSynthesis"
   constructor(private http:HttpClient) { }
 
   getTTS = text => {
-    return this.http.post<any>(this.apiUrl, { "text": text } )
+    let apiData = {
+      "synthinput": {
+        "text": text,
+        "ssml": "string"
+      },
+      "voiceparams": {
+        "languageCode": "ga-IE",
+        "name": "ga_UL_anb_nnmnkwii",
+        "ssmlGender": "UNSPECIFIED"
+      },
+      "audioconfig": {
+        "audioEncoding": "LINEAR16",
+        "speakingRate": 1,
+        "pitch": 1,
+        "volumeGainDb": 1,
+        "sampleRateHertz": 0,
+        "effectsProfileId": []
+      },
+      "outputType": "JSON",
+      "timing": "BOTH"
+    }
+    return this.http.post<any>(this.apiUrl, apiData)
   }
 }
