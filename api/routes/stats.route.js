40ab3c9f752bcd5dc320f3b9f5483bfad3755678
Neimhin <nrobinso@tcd.ie>
2022-04-29 14:31:28 +0100

    allow 3mb bundle size

--- api/routes/stats.route.js
+++ api/routes/stats.route.js
@@ -83,9 +83,8 @@ statsRoutes.route('/getFeatureDataByDate/:startDate/:endDate').get((req, res) =>
     }
     else {
       res.status(200).json(events);
-    }  
+    }
   });
-    
 });
 
 statsRoutes.route('/getFeatureDataSinceLog/:date').get((req, res) => {
@@ -101,9 +100,8 @@ statsRoutes.route('/getFeatureDataSinceLog/:date').get((req, res) => {
     else {
       console.log(events);
       res.status(200).json(events);
-    }  
+    }
   });
-    
 });
 
 
