60c92419df28a85d56f6e27c869492d12138eda1
Neimhin <nrobinso@tcd.ie>
2022-05-15 18:03:27 +0100

    imports for report-an-issue component tests

--- ngapp/src/report-an-issue/report-an-issue.component.spec.ts
+++ ngapp/src/report-an-issue/report-an-issue.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ReportAnIssueComponent } from './report-an-issue.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ReportAnIssueComponent', () => {
   let component: ReportAnIssueComponent;
@@ -8,6 +9,10 @@ describe('ReportAnIssueComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+      ],
       declarations: [ ReportAnIssueComponent ]
     })
     .compileComponents();
