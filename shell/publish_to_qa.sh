e73453a44ef4c07e75c137dd3fab241adab48047
Neimhin <nrobinso@tcd.ie>
2022-05-15 13:46:15 +0100

    conditional build in shell/publish_to_qa.sh

--- shell/publish_to_qa.sh
+++ shell/publish_to_qa.sh
@@ -1,29 +1,32 @@
 GREEN="\e[1;32m"
 RED="\e[1;31m"
 RESET_COLOR="\e[0m"
-set -e
-
-# BUILD QA
-npm run build:qa --prefix ngapp
 
 current_branch=`git rev-parse --abbrev-ref HEAD`
-echo $current_branch
 
-git add ngapp/qa_build
-git commit -m "autocommit ngapp/qa_build"
-git push
+# BUILD QA
+if [[ $BUILD_NG -gt 0 ]]; then
+  npm run build:qa --prefix ngapp
+  git add ngapp/qa_build
+  git commit -m "autocommit ngapp/qa_build"
+  git push
+fi
+
 
 # DEPLOY ON LIVE SERVER
 echo -e "${GREEN}PASSWORD REQUIRED$RESET_COLOR"
 ssh -t scealai@141.95.1.243 "
-  cd an_scealai_qa &&
-  git pull &&
-  git checkout $current_branch &&
-  bash shell/reinstall.sh &&
-  tmux kill-session -t qa &&
+  echo \"kill tmux session\"
+  tmux kill-session -t qa
+  cd an_scealai_qa && pwd &&
+  git pull && git rev-parse --abbrev-ref HEAD &&
+  git checkout $current_branch && git rev-parse --abbrev-ref HEAD &&
+  npm --prefix api install &&
+  npm --prefix ngapp install &&
   tmux new-session -t qa -d &&
-  tmux send -t qa \"npm run start:qa\" C-m &&
-  cp -r ngapp/qa_build/* dist/an-scealai/
+  tmux send -t qa \"npm --prefix api run start:qa\" C-m &&
+  cp -r ngapp/qa_build/* dist/an-scealai/ &&
+  TERM=xterm tmux at -t qa
   "
 
 curl https://www.abair.ie/qa/anscealaibackend/version
