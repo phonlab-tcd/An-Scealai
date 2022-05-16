10f5fae4d3f2cc72fdd10c984d823cb370ed2b2c
Neimhin <nrobinso@tcd.ie>
2022-05-16 13:50:10 +0100

    passe err to error handler

--- api/endpoints_functions/gramadoir/insert.js
+++ api/endpoints_functions/gramadoir/insert.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { upsertGramadoirCacheItem, upsertStoryGramadoirVersion} = require('../../utils/grammar');
 
-module.exports = async (req, res) => {
+module.exports = async (req, res, next) => {
   const cache_id =
     (await upsertGramadoirCacheItem(req.body.text, req.body.tagData))
     ._id;
@@ -9,5 +9,5 @@ module.exports = async (req, res) => {
     mongoose.mongo.ObjectId(req.body.storyUnderscoreId),
     mongoose.mongo.ObjectId(req.user._id),
     cache_id,
-    req.body.timestamp).catch((err) => {throw err});
+    req.body.timestamp).catch((err) => next(err));
 }
