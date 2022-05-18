bc0451642488f593d956f31516291a96cb66aa96
Neimhin <nrobinso@tcd.ie>
2022-05-18 17:24:51 +0100

    no tail in mongotest/start.sh

--- mongotest/start.sh
+++ mongotest/start.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-set -o errexit
 cat ./mongotest/mongotest.conf
 mongod \
   --config ./mongotest/mongotest.conf \
   --logpath ./mongotest/logs/log \
-  --dbpath ./mongotest/db/ &
-
-tail -f ./mongotest/logs/log
-
+  --dbpath ./mongotest/db/
