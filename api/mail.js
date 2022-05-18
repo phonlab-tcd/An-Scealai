56223c9c2ee070f2ff5c4a50cf30c329ce337ac4
Neimhin <nrobinso@tcd.ie>
2022-05-18 16:39:58 +0100

    option to turn off sendEmail function

--- api/mail.js
+++ api/mail.js
@@ -9,60 +9,57 @@ const nodemailer = require("nodemailer");
  * @param {String} subject - Subject of the email
  * @param {String} message - message
  */
-
-
 const path = require('path');
 const fs = require('fs');
 
 module.exports.sendEmail = "not yet created";
 
-try{
-  console.log("Attempting to read sendinblue auth data from ./api/sendinblue.json");
-  let rawdata = fs.readFileSync(path.join(__dirname, 'sendinblue.json'));
-  let sendinblueData = JSON.parse(rawdata);
+// send mail with defined transport object.
+if(process.env.NO_EMAILS) {
+  console.log('NO_EMAILS: sendEmail neutered');
+  module.exports.sendEmail = ()=>true;
+} else {
+  try{
+    console.log("Attempting to read sendinblue auth data from ./api/sendinblue.json");
+    let rawdata = fs.readFileSync(path.join(__dirname, 'sendinblue.json'));
+    let sendinblueData = JSON.parse(rawdata);
+    const sendEmail = async (mailObj) => {
+      const { from, recipients, subject, message } = mailObj;
+      try {
+        // Create a transporter
+        let transporter = nodemailer.createTransport({
+          host: "smtp-relay.sendinblue.com",
+          port: 587,
+          auth: {
+            user: sendinblueData.user,
+            pass: sendinblueData.pass,
+          },
+        });
 
-  const sendEmail = async (mailObj) => {
-    const { from, recipients, subject, message } = mailObj;
+        let mailStatus = await transporter.sendMail({
+          from: from,
+          to: recipients,
+          subject: subject,
+          text: message,
+        });
 
-    try {
-      // Create a transporter
-      let transporter = nodemailer.createTransport({
-        host: "smtp-relay.sendinblue.com",
-        port: 587,
-        auth: {
-          user: sendinblueData.user,
-          pass: sendinblueData.pass,
-        },
-      });
+        return mailStatus;
 
-      // send mail with defined transport object.
-      if(process.env.NO_EMAILS) return true;
-
-      let mailStatus = await transporter.sendMail({
-        from: from,
-        to: recipients,
-        subject: subject,
-        text: message,
-      });
-
-      return mailStatus;
-
-    } catch(error) {
-      console.error(error);
-      throw error;
+      } catch(error) {
+        console.error(error);
+        throw error;
+      }
     }
+
+    module.exports.sendEmail = sendEmail;
+
+  } catch(err) {
+    console.error("Failed to create email transport in ./api/mail.js. Have you created sendinblue.json ?");
+    console.error(err);
+    module.exports.sendEmail = null;
+    module.exports.couldNotCreate = true;
   }
-
-  module.exports.sendEmail = sendEmail;
-
-} catch(err) {
-  console.error("Failed to create email transport in ./api/mail.js. Have you created sendinblue.json ?");
-  console.error(err);
-  module.exports.sendEmail = null;
-  module.exports.couldNotCreate = true;
 }
-
-
 /* EXAMPLE OF HOW TO USE THE sendEmail function
 const mailObj = {
   from: "nrobinso@tcd.ie",
