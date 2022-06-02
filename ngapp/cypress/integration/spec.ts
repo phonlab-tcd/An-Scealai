93bd67af5f619b83d4a02678831920c7bd5c84ea
Neimhin <nrobinso@tcd.ie>
2022-06-02 21:48:17 +0100

    Revert "expanded cypress flow"
    This reverts commit 47f3f287a90e9cd8c48fce752089553bb621989e.

--- ngapp/cypress/integration/spec.ts
+++ ngapp/cypress/integration/spec.ts
@@ -20,9 +20,6 @@ function fudgeVerification(username: string) {
 
 describe('Register Teacher', () => {
   it('Visits the initial project page', () => {
-    const username = randomString();
-    const password = randomString();
-    const email = `${randomString(10)}@${randomString(10)}.${randomString(5,false)}`;
     cy.visit('/')
     cy.url().should('match', /\/landing$/);
     cy.get('img[class="landingImg"]').should('be.visible');;
@@ -30,9 +27,11 @@ describe('Register Teacher', () => {
     cy.get('label').click();
     cy.get('button[class="btn navbarBtn"]').contains('English').click();
     cy.get('button[class="btn teacherRegisterBtn"]').contains('I am a teacher').click();
+    const username = randomString();
     cy.get('input[name="username"]').type(username);
-    cy.get('input[name="email"]').type(email);
+    cy.get('input[name="email"]').type(`${randomString(10)}@${randomString(10)}.${randomString(5,false)}`);
     cy.get('input[name="password"]').then(pword=>{
+      const password = randomString();
       [...pword].forEach(p=>{
         cy.wrap(p).type(password);
       })
@@ -41,11 +40,5 @@ describe('Register Teacher', () => {
     fudgeVerification(username);
     cy.get('button.loginBtn').contains('Sign in').click();
     cy.get('button i.fa-user').click();
-    cy.get('button').contains('Sign out').click();
-    cy.get('.loginTextContainer a')
-      .contains('Sign in').click();
-    cy.get('input[name="username"]').type(username);
-    cy.get('input[name="password"]').type(password);
-    cy.get('button.loginBtn').click();
   })
 })
