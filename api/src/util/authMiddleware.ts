391a89212eb2fd7f16feca5a3805b527bb2e3321
Neimhin <nrobinso@tcd.ie>
2022-06-03 14:52:39 +0100

    authMiddleware as .ts

--- api/src/util/authMiddleware.ts
+++ api/src/util/authMiddleware.ts
@@ -1,3 +1,3 @@
 const passport = require('passport');
-module.exports.jwtmw = passport.authenticate('jwt',{session: false});
+module.exports.jwtmw      = passport.authenticate('jwt',{session: false});
 module.exports.passwordmw = passport.authenticate('local');
