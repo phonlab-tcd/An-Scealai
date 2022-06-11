d309bfce93e8fbce9b637db84760673b7966be84
Neimhin <nrobinso@tcd.ie>
2022-06-11 12:44:20 +0100

    collect coverage

--- api/jest.config.js
+++ api/jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: '@shelf/jest-mongodb',
   setupFiles: [ "<rootDir>/utils/test-setup.js" ],
   setupFilesAfterEnv: ["<rootDir>/utils/test-after-env.js"],
+  collectCoverage: true,
 };
