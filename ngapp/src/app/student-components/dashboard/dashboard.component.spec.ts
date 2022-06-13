5e0ff42818f26442ca2ce2a70b6e080153c4332e
Neimhin <nrobinso@tcd.ie>
2022-06-13 11:09:45 +0100

    jasmine test student dashboard initialization

--- ngapp/src/app/student-components/dashboard/dashboard.component.spec.ts
+++ ngapp/src/app/student-components/dashboard/dashboard.component.spec.ts
@@ -3,16 +3,26 @@ import { DashboardComponent } from './dashboard.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
+import { QuillModule } from 'ngx-quill';
+import { TranslationService } from '../../translation.service';
+import { CommonModule } from '@angular/common';
 
-describe('DashboardComponent', () => {
+fdescribe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       schemas: [ NO_ERRORS_SCHEMA ],
-      imports: [RouterTestingModule, HttpClientTestingModule],
-      declarations: [ DashboardComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        QuillModule,
+        CommonModule,
+      ],
+      declarations: [ DashboardComponent ],
+      providers: [ TranslationService ]
     })
     .compileComponents();
   }));
@@ -33,5 +43,45 @@ describe('DashboardComponent', () => {
         expect(component.saveStoryDebounceId).toBe(0)
         done()
       },300);
+  });
+
+  it('should have a title', ()=>{
+    const title = fixture.debugElement.query(By.css('[data-cy=title]'));
+    console.log(title.nativeElement.innerText);
+    expect(title.nativeElement.innerText).toEqual('...');
+  });
+
+  it('should display the story\'s title', async ()=>{
+    const id = 1234;
+    spyOn(component,'getStories').and
+      .returnValue(Promise.resolve([{title: 'hello',_id: id,text:'text'}]));
+    spyOn(component,'getStoryId').and
+      .returnValue(Promise.resolve(id));
+    await component.ngOnInit()
+    fixture.detectChanges();
+    const title = fixture.debugElement.query(By.css('[data-cy=title]'));
+    expect(title.nativeElement.innerText).toEqual('hello');
+  });
+
+  it('should display the story\'s text', async ()=>{
+    const id = 1234;
+    spyOn(component,'getStories').and
+      .returnValue(Promise.resolve([{
+        title: 'hello',
+        _id: id,
+        htmlText:'sillytext',
+        text:'sillytext',
+      }]));
+    spyOn(component,'getStoryId').and
+      .returnValue(Promise.resolve(id));
+    await component.ngOnInit()
+    fixture.detectChanges();
+    const editor = fixture.debugElement.query(By.css('div.ql-editor'));
+    fixture.detectChanges();
+    console.log(editor.nativeElement);
+    expect(component.story.text).toBe('sillytext');
+    expect(component.story.htmlText).toBe('sillytext');
+    console.log(editor);
+    expect(editor.nativeElement.innerText.trim()).toBe('sillytext');
   });
 });
