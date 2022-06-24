0c2e44fca196366fb54ad6e8c0c07e7d8aa44227
Neimhin <nrobinso@tcd.ie>
2022-06-24 16:04:29 +0100

    typegoose type

--- api/jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  setupFiles: ['./util/test-setup.js'],
-  moduleFileExtensions: ['js', 'mjs', 'ts'],
-};
