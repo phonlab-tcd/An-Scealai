39025ce8a22165c5dc54f175d1ef626b1a61232f
Neimhin <nrobinso@tcd.ie>
2022-07-06 16:07:46 +0100

    gramadoirViewExample allowDiskUse

--- api/mongo_scripts/gramadoirViewExample.js
+++ api/mongo_scripts/gramadoirViewExample.js
@@ -1,17 +1,11 @@
 ;(async () => {
-  return new Promise(async (resolve) => {
-    const viewName = ''+Date.now();
-    await require('../utils/gramadoirView')(viewName);
-    const mongoose = require('mongoose');
-    const { db } = mongoose.connection;
-    const cursor = db.collection(viewName).find();
-    cursor.toArray((err,docs)=>{
-      console.error(err);
-      docs.forEach(d=>{
-        console.log(d);
-      });
-      db.collection(viewName).drop();
-      resolve();
-    });
-  });
+  const viewName = ''+Date.now();
+  await require('../utils/gramadoirView')(viewName);
+  const mongoose = require('mongoose');
+  const { db } = mongoose.connection;
+  const cursor = db.collection(viewName).find({},{allowDiskUse:true});
+  const docs = await cursor.toArray();
+  docs[200].gramadoirHistory.map(a=>console.dir(a.gramadoir));
+  db.collection(viewName).drop();
+  process.exit(0);
 })();
