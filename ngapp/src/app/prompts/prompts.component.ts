9df88aae826afb10ce640a26f6b6c3182c999fff
FionnCL <camacholenihanfionn@gmail.com>
2022-08-09 16:04:11 +0100

    Deleted Comments

--- ngapp/src/app/prompts/prompts.component.ts
+++ ngapp/src/app/prompts/prompts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, SimpleChanges, ViewEncapsulation,  } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation,  } from '@angular/core';
 import { GrammarService } from 'app/grammar.service';
 import { TranslationService } from '../translation.service';
 import { StoryService } from '../story.service'
@@ -29,24 +29,6 @@ export class PromptsComponent implements OnInit {
     conjunction: ['agus', 'go'],
     numeral:     ['trí', 'céad', 'triúr', 'ceathrar']
   }
-
-  //This is for the highlighting of buttons based on whether they can be used next in the sentence.
-  //It is not so easy with such loosely defined word types. I have also never done computational morphology before.
-  //Don't have the following properly defined in tempWordDatabase: subject, object, 
-  //'dir.object', 'ind.object', 'local modifier', 'modal modifier', 'temportal modifier''etc.', 'dir.pronominal object'
-  /**
-  subject: string[] = ['pronoun', 'noun'];
-  object: string[] = ['pronoun', 'noun'];
-
-  wordOrder = [
-    ['verb', this.subject, this.object],
-    ['verb', this.subject, 'dir.object', 'ind.object', 'local modifier', 'modal modifier', 'temportal modifier'],
-    ['verb', this.subject, 'ind.object', 'etc.', 'dir.pronominal object'],
-  ]
-   */
-  //I realize there is too much nuance between words that would take too long to accomodate for, I will try
-  //it the slow way using Gramadoir.
-
   
   /** tempWordDatabaseEnglish = {
     noun:        ['the kitchen', 'the floor', 'the man', 'the boat'],
@@ -61,10 +43,6 @@ export class PromptsComponent implements OnInit {
     numeral:     ['3', '100', '3(Person)', '4(People)']
   } */
 
-
-  // @ViewChild('audioElement') audioElement; // unused
-
-  //Unsure whether some of this stuff should be in the constructor.
   WORD_PROMPT = 'Please choose a word type';
   givenWord = 'Please choose a word type';
   wordBank = [];
@@ -116,35 +94,6 @@ export class PromptsComponent implements OnInit {
   async selectWord(type: keyof typeof this.tempWordDatabase) {
     this.givenWord  = this.randomWord(this.tempWordDatabase[type]);
   }
-
-  //Slow way of checking first word in each word type list to give button "viability" color.
-  /** Note: Doesn't work currently, just leaving here incase I use it. Even if it did work however,
-   * it would be extremely slow most likely
-  async buttonHighlighting() {
-    this.buttonsLoading = true;
-    for(let key in this.tempWordDatabase){
-      let first = this.tempWordDatabase[key][0];
-      let next = this.gs.gramadoirDirectObservable(this.arrayString + " " + first, 'ga');
-      let error;
-      next.subscribe(res => {
-      console.log(res,'<CHECKING FOR ERRORS>');
-        error = res.map(element => 
-          new Object(
-            {
-              errorText: String(element.errortext)
-            }
-          )
-        )
-      });
-      //if(error[-1].errorText !== "undefined" || undefined) {
-        this.errorButtons.push(this.wordTypes[-1]);
-        console.log(this.errorButtons, '(NEW BUTTON ADDED)');
-      //} else {
-      //  console.log('(NO ERROR)');
-      //}
-      //console.log(error[0].errorText,"<ERROR ERROR ERROR>");
-    }
-  } */
 
   posConfirmation(isConfirmed: Boolean) {
     if(isConfirmed && this.givenWord != this.WORD_PROMPT){
