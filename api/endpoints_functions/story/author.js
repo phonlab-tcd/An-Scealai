1f508b58e2233b0ca69239a3db75470b5e664a5f
Neimhin <nrobinso@tcd.ie>
2022-08-13 20:54:30 +0100

    comment DEPRECATE /story/:author endpoint

--- api/endpoints_functions/story/author.js
+++ api/endpoints_functions/story/author.js
@@ -1,7 +1,7 @@
 const Story = require('../../models/story');
 const {API404Error} = require('../../utils/APIError');
 
-/**
+/** // DEPRECATED
  * @returns a list of stories written by the 'author' param
  */
 module.exports = async (req, res) => {
