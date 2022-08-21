932991b00ad54d78655ff4e91f987692155d0530
Oisín <oinolan@tcd.ie>
2022-08-21 11:51:13 +0100

    Only add '@' if there are mongo login details

--- api/utils/dbUrl.js
+++ api/utils/dbUrl.js
@@ -1,8 +1,9 @@
 const config = require('../DB');
+
 // use the URL for the test DB if it has been set, otherwise use the normal DB.
 module.exports =
   process.env.TEST_MONGO_URL ||
   (process.env.DB_URL_PREFIX   || config.DB_URL_PREFIX   ) +
-  (process.env.DB_AUTH_DETAILS || config.DB_AUTH_DETAILS ) + '@' +
+  (process.env.DB_AUTH_DETAILS ? (process.env.DB_AUTH_DETAILS + '@') : config.DB_AUTH_DETAILS ) +
   (process.env.DB_HOSTNAME     || config.DB_HOSTNAME     ) +
   (process.env.DB              || config.DB              );
