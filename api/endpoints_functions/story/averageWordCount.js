ba9fc1abff22d183af018c2771092381cb3399cd
Maddie <13711456+maddiecomtois@users.noreply.github.com>
2022-09-20 12:53:53 +0100

    add carousel

--- api/endpoints_functions/story/averageWordCount.js
+++ api/endpoints_functions/story/averageWordCount.js
@@ -1,15 +1,17 @@
 const Story = require('../../models/story');
 
+/**
+ * Add two numbers.
+ * @param {Object} req The student's id number
+ * @param {Object} res The object to store the response
+ * @return {Object} Student's average word count
+ */
 module.exports = async (req, res) => {
-  console.log('Function reached!!!!!!!!!');
-  console.log('StudentId: ', req.params.studentId);
   const stories = await Story.find({'studentId': req.params.studentId});
-  console.log('stories: ', stories);
   if (stories.length > 0) {
     const wordCounts = [];
 
     for (let i = 0; i < stories.length; i++) {
-      console.log(stories[i].author);
       const words = [];
       const str = stories[i].text.replace(/[\t\n\r\.\?\!]/gm, ' ').split(' ');
       str.map((s) => {
@@ -18,14 +20,11 @@ module.exports = async (req, res) => {
           words.push(trimStr);
         }
       });
-      console.log('Words: ', words);
       wordCounts.push(words.length);
     }
 
-    console.log('Word counts: ', wordCounts);
-    const average = Math.round(wordCounts.reduce((a, b) => a + b, 0)
-     / wordCounts.length);
-    console.log('Average: ', average);
+    const average = Math.round(wordCounts.reduce((a, b) => a + b, 0) /
+     wordCounts.length);
 
     res.status(200).json({avgWordCount: average});
   } else {
