07aef637a6ec1a8b94fc75d8736e80e5d6317c4f
Maddie <13711456+maddiecomtois@users.noreply.github.com>
2022-09-23 13:15:24 +0100

    pass data through dashboard component

--- ngapp/src/app/story-stats/word-counts/word-counts/word-counts.component.ts
+++ ngapp/src/app/story-stats/word-counts/word-counts/word-counts.component.ts
@@ -1,11 +1,5 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 import { Chart } from 'chart.js';
-import { firstValueFrom } from 'rxjs';
-import { StoryService } from '../../../story.service';
-import { AuthenticationService } from '../../../authentication.service';
-import { ClassroomService } from '../../../classroom.service';
-import { UserService } from '../../../user.service';
-
 
 const LIGHT_RED = 'rgba(255, 204, 203, 1)';
 const RED = 'rgba(255, 114, 111, 1)';
@@ -19,66 +13,33 @@ const RED = 'rgba(255, 114, 111, 1)';
 })
 
 export class WordCountsComponent implements OnInit {
-
-  constructor(private storyService: StoryService, private auth: AuthenticationService,
-  private classroomService: ClassroomService, private userService: UserService) { }
   
-  classrooms:any;
-  stats:any[] = [];
+  @Input() data:any;
+
+  constructor() { }
+  
   charts:any[] = [];
 
   async ngOnInit() {
-    
-    await this.getStats();
+    console.log(this.data);
     await this.makeCharts();
-  }
-  
-  // For each classroom of logged-in teacher, get average word count for each student (over all stories)
-  private async getStats() {
-    
-    this.classrooms = await firstValueFrom(this.classroomService.getClassroomsForTeacher(this.auth.getUserDetails()._id));
-
-    for (let entry in this.classrooms) {
-      // only consider classrooms that have at least one student
-      if (this.classrooms[entry].studentIds.length > 0) {
-        // stats object created for each classroom
-        let statsEntry = {
-          classroomTitle: this.classrooms[entry].title,
-          studentNames: [],
-          averageWordCounts: [],
-          chartId: "chartId_" + entry
-        }
-        
-        // get student usernames and word count averages
-        for (let key in this.classrooms[entry].studentIds) {
-          const userId = this.classrooms[entry].studentIds[key];
-          const usernameResponse = await firstValueFrom(this.userService.getUserById(userId));
-          statsEntry.studentNames.push(usernameResponse.username);
-
-          const wordCountResponse = await firstValueFrom(this.storyService.averageWordCount(userId));
-          statsEntry.averageWordCounts.push(wordCountResponse.avgWordCount);
-        }
-          console.log(statsEntry);  
-          this.stats.push(statsEntry);
-      }
-    }
   }
   
   private async makeCharts() {
     // wait for HTML to render before adding charts (need dynamically created ids)
     setTimeout(() => {
-      for (let entry in this.stats) {
-        let canvasElem = document.getElementById(this.stats[entry].chartId) as HTMLCanvasElement;
+      for (let entry in this.data) {
+        let canvasElem = document.getElementById(this.data[entry].chartId) as HTMLCanvasElement;
         let ctx = canvasElem.getContext('2d');
         let myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: this.stats[entry].studentNames,
+                labels: this.data[entry].studentNames,
                 datasets: [{
-                    label: 'Average Word Counts For ' + this.stats[entry].classroomTitle,
-                    data: this.stats[entry].averageWordCounts,
-                    backgroundColor: this.stats[entry].averageWordCounts.map(_ => LIGHT_RED),
-                    borderColor: this.stats[entry].averageWordCounts.map(_ => RED),
+                    label: 'Average Word Counts For ' + this.data[entry].classroomTitle,
+                    data: this.data[entry].averageWordCounts,
+                    backgroundColor: this.data[entry].averageWordCounts.map(_ => LIGHT_RED),
+                    borderColor: this.data[entry].averageWordCounts.map(_ => RED),
                     borderWidth: 1
                 }]
             },
