4c3e328b884a51f9e1ce9afb7022fcfacd76fce2
Maddie <13711456+maddiecomtois@users.noreply.github.com>
2022-10-28 13:15:34 +0100

    add types

---
+++ ngapp/src/app/lib/scealai-grammar/types.ts
@@ -0,0 +1,24 @@
+export type GrammarChecker = {
+  check: (input: string) => ErrorTags[];
+  name: string;
+};
+
+export type ErrorTag = {
+  errorText: string;  // for counting
+  messageGA: string;
+  messageEN: string;
+  context: string;   // for analysis
+  type: ErrorType;
+  color: string;
+  fromX: number;
+  toX: number;
+}
+
+type ErrorType = keyof typeof errors;
+
+const errors = {
+                "URU": {color:, messageEng:, messageGA:},
+                "SEIMHIU": {color:, messageEng:, messageGA:},
+                "CEOL": {color:, messageEng:, messageGA:},
+              } as const;
+                
