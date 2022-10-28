1ece3da1b118ba1cd003d45d5dc4b3f09973ee3b
Maddie <13711456+maddiecomtois@users.noreply.github.com>
2022-10-28 15:21:44 +0100

    update an gramadoir

--- ngapp/src/app/lib/scealai-grammar/checkers/an-gramadoir.ts
+++ ngapp/src/app/lib/scealai-grammar/checkers/an-gramadoir.ts
@@ -1,28 +1,45 @@
 import { GrammarChecker} from '../types';
 import { ErrorTag} from '../types';
 
-export const gramadoir: GrammarChecker = {
+export const anGramadoir: GrammarChecker = {
   name: "AnGramadoir",
   check: check
 }; 
 
+export type GramadoirTag = {
+  fromy: string;
+  fromx: number;
+  toy: string;
+  tox: number;
+  ruleId: string;
+  msg: string;
+  contex: string;
+  contextoffset: string;
+  errortext: string;
+  errorlength: string;
+};
+
+export type GramadoirUrl = 'https://www.abair.ie/cgi-bin/api-gramadoir-1.0.pl' | 'https://cadhan.com/api/gramadoir/1.0';
+
+const gramadoirUrl: GramadoirUrl = 'https://www.abair.ie/cgi-bin/api-gramadoir-1.0.pl';
+
 async function check(input):Promise<ErrorTag[]>{
-  return new Promise<ErrorTag[]>((resolve, reject) => {
-    const errors = getDisagreeingVowelIndices(input);
+  return new Promise<ErrorTag[]>(async (resolve, reject) => {
+    const errors = await callAnGramadoir(input, 'en');
     console.log("ERRORS: ", errors);
     let errorTags:ErrorTag[] = [];
     
     for (const error of errors) {
     
       let tag:ErrorTag = {
-        errorText: input,
+        errorText: error.errorText,
         messageGA: "string",
         messageEN: "str",
         context: input,
-        type: 'LEATHAN-CAOL',
+        type: 'URU',
         color: "color",
-        fromX: error.first,
-        toX: error.second,
+        fromX: error.fromx,
+        toX: error.tox,
       }
       
       errorTags.push(tag);
@@ -30,4 +47,30 @@ async function check(input):Promise<ErrorTag[]>{
 
     resolve(errorTags);
   });
+}
+
+async function callAnGramadoir(input: string, language: 'en' | 'ga'): Promise<GramadoirTag[]> {
+
+  const res = await fetch(gramadoirUrl, {
+       headers: {
+         'Content-Type': 'application/x-www-form-urlencoded',
+       },
+       method: 'POST',
+       body: gramadoirXWwwFormUrlencodedRequestData(input.replace(/\n/g, ' '), language)
+     });
+
+  if (res.ok) {
+    console.log(res.json());
+    return res.json();
+  }
+  
+  console.log("ERROR");
+
+  throw new Error(res.statusText);
+    
+}
+
+
+function gramadoirXWwwFormUrlencodedRequestData(input: string, language: 'en' | 'ga') {
+  return `teacs=${encodeURIComponent(input)}&teanga=${language}`;
 }
--- ngapp/src/app/lib/scealai-grammar/checkers/test-checkers.ts
+++ ngapp/src/app/lib/scealai-grammar/checkers/test-checkers.ts
@@ -1,8 +1,10 @@
 import { leathanCaolChecker } from './leathan-caol-checker';
+import { anGramadoir } from './an-gramadoir';
 
 
 async function test() {
-  console.log(await leathanCaolChecker.check("Is bealach iontach é freisin chun an Ghaeilge a dhéanamh níos inrochtana"));
+  //console.log(await leathanCaolChecker.check("Is bealach iontach é freisin chun an Ghaeilge a dhéanamh níos inrochtana"));
+  console.log(await anGramadoir.check("Bhain mé taitneamh as ár bplé an tseachtain seo, tá teicneolaíocht na hurlabha nua seo ar fad corraitheach"));
 }
 
 test();
