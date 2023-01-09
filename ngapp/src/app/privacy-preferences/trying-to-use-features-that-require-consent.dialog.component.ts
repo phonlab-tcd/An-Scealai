e526b43545c501e09b078e18a59d9ea9c82e8b97
Neimhin <nrobinso@tcd.ie>
2023-01-09 20:19:12 +0000

    remove unnecessary message

--- ngapp/src/app/privacy-preferences/trying-to-use-features-that-require-consent.dialog.component.ts
+++ ngapp/src/app/privacy-preferences/trying-to-use-features-that-require-consent.dialog.component.ts
@@ -7,9 +7,6 @@ import { TranslationService } from 'app/translation.service';
     <div mat-dialog-content>
       <transl [key]="'trying_to_use_feature_that_requires_consent'"></transl>
     </div>
-    <div mat-dialog-close routerLink="/privacy-preferences">
-      <transl [key]="'go_to_privacy_preferences_page'"></transl>
-    </div>
     <div mat-dialog-actions>
       <button mat-button mat-dialog-close routerLink="/privacy-preferences">
         <transl key=ok></transl>
