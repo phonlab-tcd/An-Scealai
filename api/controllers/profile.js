cb15f4a09ec2e0ed93302fb3051c87daecad0566
Maddie <13711456+maddiecomtois@users.noreply.github.com>
2023-01-31 13:22:42 +0000

    controller comments

--- api/controllers/profile.js
+++ api/controllers/profile.js
@@ -1,27 +1,37 @@
-var mongoose = require('mongoose');
-var User = mongoose.model('User');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
+/**
+ * Get user from the DB by ID
+ * @param {Object} req headers: user ID
+ * @param {Object} res User object
+ */
 module.exports.viewUser = function(req, res) {
-  if(!req.headers._id) {
-    res.status(404).json({"message" : "User not found"});
+  if (!req.headers._id) {
+    res.status(404).json({'message': 'User not found'});
   } else {
     User.findById(req.headers._id).exec(function(err, user) {
-      if(err) {
-        res.status(400).json({"message" : "User not found"});
+      if (err) {
+        res.status(400).json({'message': 'User not found'});
       }
-      if(user) {
+      if (user) {
         res.status(200).json(user);
       }
     });
   }
 };
 
+/**
+ * Get all teacher users from the DB
+ * @param {Object} req headers: user ID
+ * @param {Object} res List of teachers
+ */
 module.exports.getTeachers = function(req, res) {
-  User.find({'role':'TEACHER'}).exec(function(err, teachers) {
-    if(err) {
-      res.status(400).json({"message" : "Teachers not found"});
+  User.find({'role': 'TEACHER'}).exec(function(err, teachers) {
+    if (err) {
+      res.status(400).json({'message': 'Teachers not found'});
     }
-    if(teachers) {
+    if (teachers) {
       res.status(200).json(teachers);
     }
   });
