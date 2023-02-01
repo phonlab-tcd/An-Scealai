c53a9048e32aca61336ba9fa28bca5ed7931bc43
Maddie <13711456+maddiecomtois@users.noreply.github.com>
2023-02-01 11:37:03 +0000

    Update teachers.component.spec.ts

--- ngapp/src/app/admin-components/teachers/teachers.component.spec.ts
+++ ngapp/src/app/admin-components/teachers/teachers.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TeachersComponent } from './teachers.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 describe('TeachersComponent', () => {
   let component: TeachersComponent;
@@ -10,7 +11,11 @@ describe('TeachersComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule, HttpClientTestingModule],
-      declarations: [ TeachersComponent ]
+      declarations: [ TeachersComponent ],
+      providers: [
+          { provide: MAT_DIALOG_DATA, useValue: {} },
+          { provide: MatDialogRef, useValue: {} } ---> now
+      ]
     })
     .compileComponents();
   }));
