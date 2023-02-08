5a4f88427fee126648edb126cdfa3e9765498cf6
Maddie Comtois <maddie.comtois@gmail.com>
2023-02-08 14:14:20 +0000

    make separate word count function

--- api/endpoints_functions/story/getStoryStats.js
+++ api/endpoints_functions/story/getStoryStats.js
@@ -1,9 +1,6 @@
 const Story = require('../../models/story');
-const winkNLP = require('wink-nlp');
-const model = require('wink-eng-lite-web-model');
+import { getWordCount } from "../nlp/getWordCount";
 
-const nlp = winkNLP(model);
-const STOP_WORDS = ['.', ',', '?', '!', '\n', ';', '-', ':', '"', '\''];
 
 /**
  * Gets story statistics such as total stories and average word count
@@ -15,32 +12,16 @@ async function getStoryStats(req, res) {
   const totalStories = await Story.countDocuments();
   const totalRecordings = await Story.where({'activeRecording': {$ne: null}}).countDocuments();
 
-  const stories = await Story.find();
-  let avgWordCount = 0;
-  const wordCounts = [];
-  let totalWords = 0;
+  const storyTextsResponse = await Story.find({},{'text': 1, '_id': 0})  // get text from all stories in the DB
+  const storyTexts = storyTextsResponse.map( (item) => {return item.text}) // make an array of story texts from DB res
 
-  if (stories.length > 0) {
-    stories.forEach((story) => {
-      if (story.text) {
-        const textTokens = nlp
-            .readDoc(story.text)
-            .tokens()
-            .out()
-            .filter((token) => !STOP_WORDS.includes(token));
-        wordCounts.push(textTokens.length);
-        totalWords += textTokens.length;
-      }
-    });
-
-    avgWordCount = Math.round(wordCounts.reduce((a, b) => a + b, 0) / wordCounts.length);
-  }
+  const wordCountResponse = getWordCount(storyTexts);
 
   return res.status(200).json({
     totalStories: totalStories,
-    totalWords: totalWords,
-    avgWordCount: avgWordCount,
     totalRecordings: totalRecordings,
+    totalWords: wordCountResponse['totalWords'],
+    avgWordCount: wordCountResponse['avgWordCount'],
   });
 }
 
