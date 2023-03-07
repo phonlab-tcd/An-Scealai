9ca4696bf6de5b103886b72fb9ab9ef1b35fcdd1
Maddie Comtois <maddie.comtois@gmail.com>
2023-03-07 17:02:14 +0000

    clean registration module

--- ngapp/src/register/form.component.ts
+++ ngapp/src/register/form.component.ts
@@ -1,23 +1,23 @@
-import { OnInit, Component, Output, EventEmitter } from '@angular/core';
-import { TranslationService } from 'app/translation.service';
-import { AuthenticationService, RegistrationTokenPayload } from 'app/authentication.service';
-import { UntypedFormControl } from '@angular/forms';
-import config from 'abairconfig';
-import {ActivatedRoute} from "@angular/router";
+import { OnInit, Component, Output, EventEmitter } from "@angular/core";
+import { TranslationService } from "app/translation.service";
+import { AuthenticationService, RegistrationTokenPayload } from "app/authentication.service";
+import { UntypedFormControl } from "@angular/forms";
+import config from "abairconfig";
+import { ActivatedRoute } from "@angular/router";
 
-type UsernameMessageKey = 'username_no_spaces' | 'username_no_special_chars';
+type UsernameMessageKey = "username_no_spaces" | "username_no_special_chars";
 
 @Component({
-  selector: 'register-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./register.component.scss'],
+  selector: "register-form",
+  templateUrl: "./form.component.html",
+  styleUrls: ["./register.component.scss"],
   host: {
-    class: 'registerContainer',
+    class: "registerContainer",
   },
 })
 export class RegisterFormComponent implements OnInit {
-  @Output() registerSuccess:
-    EventEmitter<RegistrationTokenPayload> = new EventEmitter();
+  // credentials sent back to register component after success
+  @Output() registerSuccess: EventEmitter<RegistrationTokenPayload> = new EventEmitter();
 
   errorTextKeys: string[];
   passwordConfirm: string;
@@ -30,92 +30,114 @@ export class RegisterFormComponent implements OnInit {
 
   credentials: RegistrationTokenPayload = {
     baseurl: config.baseurl,
-    username: '', 
-    email: '',
-    password: '',
-    role: '',
-    language: 'ga',
+    username: "",
+    email: "",
+    password: "",
+    role: "",
+    language: "ga",
   };
 
   constructor(
     public ts: TranslationService,
     private auth: AuthenticationService,
-    private route: ActivatedRoute,
-  ){ }
+    private route: ActivatedRoute
+  ) {}
 
+  /**
+   * Get user role from URL params
+   * Check if username meets all criterea, update messages accordingly
+   */
   ngOnInit() {
-    this.route.params.subscribe(
-      params => {
-        if (params.role && (params.role === 'TEACHER' || params.role === 'STUDENT')) {
-          this.credentials.role = params.role;
-        }
-      },
-      error => {
-        console.error(error);
-      });
+    // check url to see if registering student or teacher
+    let role = this.route.snapshot.params["role"];
+    if (role && (role === "TEACHER" || role === "STUDENT")) {
+      this.credentials.role = role;
+    } else {
+      return;
+    }
 
-    this.usernameClass = '';
+    this.usernameClass = "";
     this.usernameErrorTextKeys = [];
     this.usernameInput = new UntypedFormControl();
+    // check if the username matches all criterea as the user is typing it in
     this.usernameInput.valueChanges.subscribe((text) => {
       this.credentials.username = text;
       this.usernameErrorTextKeys = [];
-      if (text.match(' ')) {
-        this.usernameClass = 'usernameInputRed';
-        // username shouldn't contain spaces
-        this.usernameErrorTextKeys.push('username_no_spaces') 
+      if (text.match(" ")) {
+        this.usernameClass = "usernameInputRed";
+        this.usernameErrorTextKeys.push("username_no_spaces"); // username shouldn't contain spaces
       } else if (!text.match(/^[A-Za-z0-9]+$/)) {
-        this.usernameClass = 'usernameInputRed';
-        this.usernameErrorTextKeys.push('username_no_special_chars')// 'Your username shouldn\'t contain special characters (this includes fadas unfortunately!)';
+        this.usernameClass = "usernameInputRed";
+        this.usernameErrorTextKeys.push("username_no_special_chars"); // no special characters (fadas, etc.);
       } else {
-        this.usernameClass = '';
+        this.usernameClass = "";
         this.usernameErrorTextKeys = [];
       }
     });
   }
 
+  /**
+   * Register a user and emit user details back to parent component
+   */
   register() {
     console.log("REGISTER");
     if (!this.checkDetails()) return;
-    this.credentials.language = this.ts.inIrish() ? 'ga' : 'en';
+    this.credentials.language = this.ts.inIrish() ? "ga" : "en";
     this.auth.register(this.credentials).subscribe(
-      _ok => {
-        console.log('SUCCESFUL REGISTRATION', this.credentials);
+      (_ok) => {
+        console.log("SUCCESFUL REGISTRATION", this.credentials);
         this.registerSuccess.emit(this.credentials);
       },
-      err => {
-        if(err.error && err.error.messageKeys)
+      (err) => {
+        if (err.error && err.error.messageKeys)
           this.errorTextKeys = err.error.messageKeys;
-    });
+      }
+    );
   }
 
+  /**
+   * Checks if the entered username, password, and email have no errors
+   * @returns true if no errors, otherwise false
+   */
   checkDetails(): boolean {
     this.errorTextKeys = [];
     this.checkPassword();
     this.checkEmail();
     this.checkUsername();
-    return (this.errorTextKeys.length === 0);
+    return this.errorTextKeys.length === 0;
   }
 
+  /**
+   * Check email input is in 'email' format
+   */
   checkEmail(): void {
     const emailRegex =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (! emailRegex.test(this.credentials.email))
-      this.errorTextKeys.push('email_format_error');
+    if (!emailRegex.test(this.credentials.email))
+      this.errorTextKeys.push("email_format_error");
   }
 
+  /**
+   * Check if password input is at least 5 char long and matches confirm password
+   */
   checkPassword(): void {
     if (this.credentials.password.length < 5)
-      this.errorTextKeys.push('passwords_5_char_long');
+      this.errorTextKeys.push("passwords_5_char_long");
     if (this.credentials.password !== this.passwordConfirm)
-      this.errorTextKeys.push('passwords_must_match');
+      this.errorTextKeys.push("passwords_must_match");
   }
 
+  /**
+   * Check is username input contains any special characters
+   */
   checkUsername(): void {
-    if (!this.credentials.username.match('^[A-Za-z0-9]+$'))
-      this.errorTextKeys.push('username_no_special_chars');
+    if (!this.credentials.username.match("^[A-Za-z0-9]+$"))
+      this.errorTextKeys.push("username_no_special_chars");
   }
 
+  /**
+   * Show/hide the terms and conditions
+   */
   toggleTerms() {
     this.termsVisible = !this.termsVisible;
   }
--- ngapp/src/register/waiting-for-email-verification.ts
+++ ngapp/src/register/waiting-for-email-verification.ts
@@ -1,22 +1,21 @@
-import { Component, Input, ChangeDetectorRef } from '@angular/core';
-import { TranslationService } from 'app/translation.service';
-import { EngagementService } from 'app/engagement.service';
-import { EventType } from 'app/event';
+import { Component, Input, ChangeDetectorRef } from "@angular/core";
+import { TranslationService } from "app/translation.service";
+import { EngagementService } from "app/engagement.service";
+import { EventType } from "app/event";
 import { AuthenticationService, RegistrationTokenPayload } from "app/authentication.service";
 import { Router } from "@angular/router";
 
 @Component({
-  selector: 'waiting-for-email-verification',
-  templateUrl: './waiting-for-email-verification.html',
-  styleUrls: [
-    './register.component.scss'
-  ],
+  selector: "waiting-for-email-verification",
+  templateUrl: "./waiting-for-email-verification.html",
+  styleUrls: ["./register.component.scss"],
   host: {
-    class: 'registerContainer',
+    class: "registerContainer",
   },
 })
 export class WaitingForEmailVerificationComponent {
   errorKeys: string[];
+  // user credentials passed after successful registration
   @Input() credentials: RegistrationTokenPayload;
 
   constructor(
@@ -24,20 +23,24 @@ export class WaitingForEmailVerificationComponent {
     private engagement: EngagementService,
     private auth: AuthenticationService,
     private router: Router,
-    private cd: ChangeDetectorRef,
-  ) { }
+    private cd: ChangeDetectorRef
+  ) {}
 
+  /**
+   * Logs in user with newly registered credentials
+   * @param credentials token payload sent from form.component
+   */
   login(credentials: RegistrationTokenPayload) {
     this.auth.login(credentials).subscribe(
-      _ok => {
-        console.log('LOGIN OK');
+      (_ok) => {
+        console.log("LOGIN OK");
         this.engagement.addEventForLoggedInUser(EventType.REGISTER);
         this.ts.setLanguage(this.ts.l.iso_code);
-        this.router.navigateByUrl('register-profile');
+        this.router.navigateByUrl("register-profile");
       },
-      err => {
+      (err) => {
         console.error(err);
-        console.log('LOGIN ERROR');
+        console.log("LOGIN ERROR");
         console.log(err.error);
         this.errorKeys = err.error.messageKeys;
         this.cd.detectChanges();
