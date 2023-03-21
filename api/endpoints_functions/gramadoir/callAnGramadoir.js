dd12de43cd25d2b451feec4896e85ade0dde653f
Maddie Comtois <maddie.comtois@gmail.com>
2023-03-21 18:21:57 +0000

    add node-cache

--- api/endpoints_functions/gramadoir/callAnGramadoir.js
+++ api/endpoints_functions/gramadoir/callAnGramadoir.js
@@ -1,7 +1,10 @@
 const axios = require("axios");
+const NodeCache = require("node-cache");
+
+const cache = new NodeCache({ stdTTL: 600 });
 
 /**
- * Send request to An Gramadoir for request input 
+ * Send request to An Gramadoir for request input
  * Returns any error data found
  * @param {Object} req input text to check
  * @param {Object} res
@@ -11,7 +14,7 @@ async function callAnGramadoir(req, res) {
   const url = "https://phoneticsrv3.lcs.tcd.ie/gramadoir/api-gramadoir-1.0.pl";
 
   /* encode URI params */
-  function request(text) {
+  function encodeParams(text) {
     return `teacs=${encodeURIComponent(text)}&teanga=en`;
   }
 
@@ -20,16 +23,33 @@ async function callAnGramadoir(req, res) {
     headers: { "content-type": "application/x-www-form-urlencoded" },
   };
 
-  const gramadoirRes = await axios.post(url, request(req.params["teacs"]), options).then(
-    (ok) => ({ ok }),
-    (err) => ({ err })
-  );
+  try {
+    // try to get the errors from the cache if sentence already requested
+    let cachedErrors = cache.get(req.params["teacs"]);
 
-  if ("ok" in gramadoirRes) {
-    return res.json(gramadoirRes.ok.data);
+    // if errors do not exist in the cache, request them and then set the cache
+    if (cachedErrors == null) {
+      // get errors from An Gramadoir
+      const gramadoirRes = await axios.post(url, encodeParams(req.params["teacs"]), options).then(
+        (ok) => ({ ok }),
+        (err) => ({ err })
+      );
+
+      // if response ok, set cache and return errors
+      if ("ok" in gramadoirRes) {
+        cache.set(req.params["teacs"], gramadoirRes.ok.data, 300);
+        return res.json(gramadoirRes.ok.data);
+      }
+      // otherwise return error
+      console.error(gramadoirRes.err.data);
+      return res.json(gramadoirRes.err.data);
+    }
+    // return errors stored in cache
+    res.status(200).send(cachedErrors);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
   }
-  console.error(gramadoirRes.err.data);
-  return res.json(gramadoirRes.err.data);
 }
 
 module.exports = { callAnGramadoir };
