cce1251ffdd7dbccc0778664f853c8b3fb6cd9d2
Maddie Comtois <maddie.comtois@gmail.com>
2023-04-17 12:21:10 +0100

    build

--- ngapp/src/abairconfig.qa.ts
+++ ngapp/src/abairconfig.qa.ts
@@ -1,4 +1,3 @@
 export default {
-    // baseurl: "https://www.abair.ie/qa/anscealaibackend/"
-    baseurl: "https://www.abair.ie/qa/scealai/"
+    baseurl: "https://www.abair.ie/qa/anscealaibackend/"
 }
