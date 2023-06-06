982c878f143ebe4d47fee1971ff5a8d04faf3ab7
Oisín <oinolan@tcd.ie>
2023-06-06 18:07:26 +0100

    Monkey-patch console.error

--- ngapp/src/app/app.component.ts
+++ ngapp/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { NavigationEnd } from "@angular/router";
 import { Router } from "@angular/router";
 import { filter } from "rxjs/operators";
+import { LogService } from 'app/core/services/log.service';
 
 declare var gtag;
 
@@ -14,10 +15,21 @@ export class AppComponent {
 
   constructor(
     private _router: Router,
+    private log: LogService
   ) {
+    const originalConsoleError = console.error;
     this._router.routeReuseStrategy.shouldReuseRoute = () => false;
-    this._router.events.subscribe((_) => {});
-
+    this._router.events.subscribe((routeEvent) => {
+      if (routeEvent instanceof NavigationEnd) {
+        console.error = (consoleError => function () {
+          const route = routeEvent.urlAfterRedirects
+          const error = arguments[1];
+          log.clientsideError(route, error.message).subscribe();
+        
+          consoleError.apply(this, arguments);
+        })(originalConsoleError);
+      }
+    });
     // add google analytics tag
     const navEndEvents = this._router.events.pipe(
       filter((event) => event instanceof NavigationEnd)
