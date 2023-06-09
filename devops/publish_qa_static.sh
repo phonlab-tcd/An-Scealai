7a39e240b70315ffe7a28faf55e37ab84d38834d
Neimhin <norbinso@tcd.ie>
2023-06-09 18:04:29 +0000

    deploy scripts WIP

--- devops/publish_qa_static.sh
+++ devops/publish_qa_static.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # NOTE: trailing slash in ngapp/qa_build/ causes rsync to copy contents of qa_build to an-scealai, without keeping top level name `qa_build`
 # i.e. ngapp/qa_build/x.txt gets moved to /home/scealai/an_scealai_qa/dist/an-scealai/x.txt
-rsync -r ngapp/qa_build/ scealai@141.95.1.243:/home/scealai/an_scealai_qa/dist/an-scealai
+
+
+# first backup live version to local device
+rsync -r --info=progress2 scealai@141.95.1.243:/home/scealai/an_scealai_qa/dist/an-scealai remote-qa-build-backup
+rsync -r --info=progress2 ngapp/qa_build/ scealai@141.95.1.243:/home/scealai/an_scealai_qa/dist/an-scealai
