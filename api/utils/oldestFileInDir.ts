fce1958454250970379671cc247e6f816155a2c1
Neimhin <norbinso@tcd.ie>
2023-06-09 14:00:39 +0000

    edge case in oldestFileInDir.ts

--- api/utils/oldestFileInDir.ts
+++ api/utils/oldestFileInDir.ts
@@ -15,6 +15,9 @@ export default async function oldestFileInDir(dir): Promise<PathLike | false> {
             time: await fs.stat(path.join(dir,fileName)).then(stats=>stats.mtime.getTime()),
         };
     }));
+    if(!fileTimes[0]){
+        return false;
+    }
     const oldestName = fileTimes.reduce((a, b) => a.time < b.time ? a : b, fileTimes[0]).name
     return path.join(dir,oldestName);
 }
