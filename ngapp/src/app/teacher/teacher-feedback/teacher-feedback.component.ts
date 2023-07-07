d04a4101d5391d840de587b88f10bb7d856e716d
Maddie Comtois <maddie.comtois@gmail.com>
2023-07-07 15:43:26 +0100

    create inline comments

--- ngapp/src/app/teacher/teacher-feedback/teacher-feedback.component.ts
+++ ngapp/src/app/teacher/teacher-feedback/teacher-feedback.component.ts
@@ -1,35 +1,33 @@
-import { Component, OnInit, ViewEncapsulation, Input, Output, EventEmitter } from "@angular/core";
-import { Router, ActivatedRoute } from "@angular/router";
-import { HttpClient } from "@angular/common/http";
+import { Component, OnInit, ViewEncapsulation, Input, Output, EventEmitter, } from "@angular/core";
+import { Router } from "@angular/router";
 import { StoryService } from "app/core/services/story.service";
 import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
 import { TranslationService } from "app/core/services/translation.service";
 import { UserService } from "../../core/services/user.service";
-import { ProfileService } from "app/core/services/profile.service";
 import { AuthenticationService } from "app/core/services/authentication.service";
 import { MatDialog, MatDialogRef } from "@angular/material/dialog";
 import { RecordingDialogComponent } from "../../dialogs/recording-dialog/recording-dialog.component";
 import { BasicDialogComponent } from "app/dialogs/basic-dialog/basic-dialog.component";
-import { Story } from 'app/core/models/story';
+import { Story } from "app/core/models/story";
 import Quill from "quill";
 import config from "abairconfig";
 
 @Component({
   selector: "app-teacher-feedback",
   templateUrl: "./teacher-feedback.component.html",
-  styleUrls: ["./teacher-feedback.component.scss", "./../../../quill.fonts.scss"],
+  styleUrls: [
+    "./teacher-feedback.component.scss",
+    "./../../../quill.fonts.scss",
+  ],
   encapsulation: ViewEncapsulation.None,
 })
 export class TeacherFeedbackComponent implements OnInit {
   constructor(
-    private route: ActivatedRoute,
-    private http: HttpClient,
     private storyService: StoryService,
     protected sanitizer: DomSanitizer,
     public ts: TranslationService,
     private router: Router,
     private userService: UserService,
-    private profileService: ProfileService,
     private auth: AuthenticationService,
     private dialog: MatDialog
   ) {}
@@ -57,22 +55,23 @@ export class TeacherFeedbackComponent implements OnInit {
   commentsList: any = [];
 
   story = {
-    _id: '1111',
-    title: 'story title',
+    _id: "1111",
+    title: "story title",
     date: new Date(),
     lastUpdated: new Date(),
-    dialect: 'munster',
-    text: 'this is a test',
-    htmlText: 'this is a test',
-    author: 'nuage',
-    studentId: '11111',
+    dialect: "munster",
+    text: "this is a test",
+    htmlText: "this is a test",
+    author: "nuage",
+    studentId: "11111",
     feedback: {
-        seenByStudent: false,
-        text: 'great job',
-        feedbackMarkup: 'Tá an ceathrú dáileog den vacsaín le tairiscint do gach duine sa stát atá 65 nó níos sine. Dheimhnigh an tAire Sláinte Stephen Donnelly ar maidin gurb í sin comhairle NIAC, an Coiste Comhairleach Náisiúnta um Imdhíonadh, agus gur ghlac sé leis an gcomhairle sin. Ag labhairt dó ar Morning Ireland ar RTÉ, dúirt Donnelly go raibh Feidhmeannacht na Seirbhíse Sláinte ag ullmhú cheana féin chun an ceathrú snáthaid a dháileadh. De réir chomhairle NIAC ba chóir an dara teanndáileog a thabhairt do dhaoine atá 12 nó níos sine atá imdhíon-lagaithe. Ba chóir a deir siad bundáileog trí shnáthaid a thabhairt do pháistí atá idir 5-11 atá imdhíon-lagaithe. Maidir leis an gceathrú snáthaid do dhaoine os cionn 65 is í comhairle NIAC ná go bhfágfaí sé mhí idir an tríú dáileog agus an ceann nua, ach go deir siad bhféadfadh go mbeadh ceithre mhí inmholta i gcásanna áirithe. Dúirt Stephen Donnelly go rabhthas ag súil moltaí NIAC a chur i gcrích “an sciobtha”. Dhéanfaí scagadh anois ar ar cheart vacsaín eile a dháileadh ar dhaoine in aoisghrúpaí eile, a dúirt sé.',
-        audioId: '11111',
+      seenByStudent: false,
+      text: "great job",
+      feedbackMarkup:
+        "Tá an ceathrú dáileog den vacsaín le tairiscint do gach duine sa stát atá 65 nó níos sine. Dheimhnigh an tAire Sláinte Stephen Donnelly ar maidin gurb í sin comhairle NIAC, an Coiste Comhairleach Náisiúnta um Imdhíonadh, agus gur ghlac sé leis an gcomhairle sin. Ag labhairt dó ar Morning Ireland ar RTÉ, dúirt Donnelly go raibh Feidhmeannacht na Seirbhíse Sláinte ag ullmhú cheana féin chun an ceathrú snáthaid a dháileadh. De réir chomhairle NIAC ba chóir an dara teanndáileog a thabhairt do dhaoine atá 12 nó níos sine atá imdhíon-lagaithe. Ba chóir a deir siad bundáileog trí shnáthaid a thabhairt do pháistí atá idir 5-11 atá imdhíon-lagaithe. Maidir leis an gceathrú snáthaid do dhaoine os cionn 65 is í comhairle NIAC ná go bhfágfaí sé mhí idir an tríú dáileog agus an ceann nua, ach go deir siad bhféadfadh go mbeadh ceithre mhí inmholta i gcásanna áirithe. Dúirt Stephen Donnelly go rabhthas ag súil moltaí NIAC a chur i gcrích “an sciobtha”. Dhéanfaí scagadh anois ar ar cheart vacsaín eile a dháileadh ar dhaoine in aoisghrúpaí eile, a dúirt sé.",
+      audioId: "11111",
     },
-    activeRecording: '1111',
+    activeRecording: "1111",
     createdWithPrompts: false,
   };
 
@@ -81,39 +80,38 @@ export class TeacherFeedbackComponent implements OnInit {
    */
   createComment() {
     const commentInput = window.prompt("Please enter Comment", "");
-  
+
     if (commentInput != null && commentInput !== "") {
       let range = this.quillEditor.getSelection();
 
       // comment regarding entire text, no highlighting applied
       if (!range || range.length === 0) {
-        range = {index: 0, length: 0}
+        range = { index: 0, length: 0 };
       }
 
       let commentObj = { range: range, comment: commentInput };
       this.commentsList.push(commentObj);
 
-      this.quillEditor.formatText(range.index, range.length, {
-        background: "#fff72b"
-      });
+      this.quillEditor.formatText(range.index, range.length, { background: "#fff72b", });
 
-      this.drawComment(commentObj.comment);
-    } 
+      this.addComment(commentObj.comment);
+    }
   }
-
 
   /**
    * Adds the comment ot this list of comments on the side of the editor
    * @param comment comment text to show on screen
    */
-  drawComment(comment: string) {
+  addComment(comment: string) {
     const commentContainer = document.getElementById("comments-container");
     if (commentContainer) {
-      const buttonElement = document.createElement('div');
+      const buttonElement = document.createElement("div");
       buttonElement.textContent = comment;
-      buttonElement.setAttribute('data-index', (this.commentsList.length - 1).toString());
-      buttonElement.classList.add('comment')
-      buttonElement.addEventListener('click', (event) => this.showCommentInQuill(event.target as HTMLElement));
+      buttonElement.setAttribute( "data-index", (this.commentsList.length - 1).toString() );
+      buttonElement.classList.add("comment");
+      buttonElement.addEventListener("click", (event) =>
+        this.highlightCommentReferenceInQuill(event.target as HTMLElement)
+      );
       commentContainer.appendChild(buttonElement);
     }
   }
@@ -122,15 +120,61 @@ export class TeacherFeedbackComponent implements OnInit {
    * Show where in quill the clicked comment is refering to
    * @param commentElement comment HTML element clicked on
    */
-  showCommentInQuill(commentElement: HTMLElement) {
-    let index = commentElement.getAttribute('data-index');
+  highlightCommentReferenceInQuill(commentElement: HTMLElement) {
+    let index = commentElement.getAttribute("data-index");
     let commentData = this.commentsList[index];
-    this.quillEditor.setSelection(commentData.range.index, commentData.range.length);
+    this.quillEditor.setSelection( commentData.range.index, commentData.range.length );
   }
 
+  /**
+   * Create and display a comment button when the user selects a range of text
+   * @param event quill on-select event
+   */
+  showInlineCommentButton(event) {
+    let range = this.quillEditor.getSelection();
+    if (range && range.length > 0 && event.source == "user") {  // don't want to create button when showCommentInQuill() is fired
+      const length = range.length;
+      // get bounds of selected text
+      const bounds = this.quillEditor.getBounds(range.index, length);
+      // get bounds of entire quill editor
+      const editorContainer = this.quillEditor.root.parentNode as HTMLElement;
+      const { top } = editorContainer.getBoundingClientRect();
 
+      // delete any existing comment buttons
+      this.deleteExistingCommentButton();
 
+      const buttonElement = document.createElement("button");
+      buttonElement.addEventListener("click", () => {
+        this.createComment();
+        this.deleteExistingCommentButton();
+      });
+      buttonElement.style.position = "absolute";
+      buttonElement.id = "commentButton";
+      buttonElement.style.left = `${bounds.right}px`;
+      buttonElement.style.top = `${top + bounds.bottom}px`;
+      buttonElement.style.zIndex = "999";
 
+      // create icon inside button
+      const iconElement = document.createElement("i");
+      iconElement.classList.add("fa", "fa-envelope");
+      buttonElement.appendChild(iconElement);
+
+      document.body.appendChild(buttonElement);
+    } else {
+      // remove any previous comment button added
+      this.deleteExistingCommentButton();
+    }
+  }
+
+  /**
+   * Remove any existing comment buttons from the DOM
+   */
+  deleteExistingCommentButton() {
+    let buttonElement = document.getElementById("commentButton");
+    if (buttonElement) {
+      buttonElement.remove();
+    }
+  }
 
   ngOnInit() {
     const userDetails = this.auth.getUserDetails();
@@ -165,12 +209,11 @@ export class TeacherFeedbackComponent implements OnInit {
       this.getAuthorPossessive();
       this.getUserId();
     }
-
   }
 
-  /* 
-  * Returns true if the texts are different, otherwise return false 
-  */
+  /*
+   * Returns true if the texts are different, otherwise return false
+   */
   checkTextDifference(text1: string, text2: string) {
     let stripped1 = text1.replace(/(<([^>]+)>)/gi, "").replace(/[\s,\.]+/g, "");
     let stripped2 = text2.replace(/(<([^>]+)>)/gi, "").replace(/[\s,\.]+/g, "");
@@ -254,7 +297,10 @@ export class TeacherFeedbackComponent implements OnInit {
     this.dialogRef = this.dialog.open(BasicDialogComponent, {
       data: {
         title: this.ts.l.story_updated,
-        message: (this.ts.l.student_has_made_changes_since_feedback).replace('#', this.story.author),
+        message: this.ts.l.student_has_made_changes_since_feedback.replace(
+          "#",
+          this.story.author
+        ),
         confirmText: this.ts.l.yes,
         cancelText: this.ts.l.no,
       },
@@ -272,17 +318,17 @@ export class TeacherFeedbackComponent implements OnInit {
     });
   }
 
-  /* 
-  * Initialise quill editor 
-  */
+  /*
+   * Initialise quill editor
+   */
   onEditorCreated(q: Quill) {
     this.quillEditor = q;
     this.quillEditor.root.setAttribute("spellcheck", "false");
   }
 
   /*
-  * Check if any changes need to be saved and prompt, otherwise go back
-  */
+   * Check if any changes need to be saved and prompt, otherwise go back
+   */
   goBack() {
     if (this.initialMarkupText !== this.story.feedback.feedbackMarkup) {
       this.dialogRef = this.dialog.open(BasicDialogComponent, {
