9f8c272d8b4be135989d24603477d60a7edca079
Neimhin <norbinso@tcd.ie>
2023-07-12 12:03:35 +0000

    TODO

--- api/endpoints_functions/user/countTeachersWithClassrooms.ts
+++ api/endpoints_functions/user/countTeachersWithClassrooms.ts
@@ -1,5 +1,6 @@
-const User = require('../../models/user');
-const Classroom = require('../../models/classroom');
+import User from "../../models/user";
+import Classroom from "../../models/classroom";
+import { Request, Response } from "express";
 
 /**
 * Returns the number of teachers who have at least one classroom
@@ -7,15 +8,16 @@ const Classroom = require('../../models/classroom');
 * @param {Object} res
 * @return {Promise} Number of teachers who have at least one classroom
 */
-async function countTeachersWithClassrooms(req, res) {
+async function countTeachersWithClassrooms(req: Request, res: Response) {
   const userIds = await User.find({'role': 'TEACHER'}, {'_id': 1});
-  const ids = userIds.map( (item) => {return item._id});
+  const ids = userIds.map(u=>u._id);
 
   let teachersWithClassrooms = 0;
 
   // count the number of users that have not written any stories
-  for (const id of ids) {
-    const numOfClassrooms = await Classroom.find({'teacherId': id}).countDocuments();
+  // TODO long for loop could be a Promise.all
+  for (const teacherId of ids) {
+    const numOfClassrooms = await Classroom.find({teacherId}).countDocuments();
 
     if (numOfClassrooms > 0) {
       teachersWithClassrooms++;
