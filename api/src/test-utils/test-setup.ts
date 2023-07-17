9874f4df4ec8a08550e6373d7e1eaf4180dc1905
Neimhin <norbinso@tcd.ie>
2023-07-17 13:14:11 +0000

    fix webpack errors

--- api/src/test-utils/test-setup.ts
+++ api/src/test-utils/test-setup.ts
@@ -1,3 +1,4 @@
+import { jest } from "@jest/globals";
 require('../keys/dev/load');
 const config = require('../DB');
 jest.setTimeout(10000);
--- api/src/utils/test-utils.js
+++ api/src/utils/test-utils.js
@@ -15,7 +15,7 @@ async function removeAllCollections() {
   const collections = Object.keys(mongoose.connection.collections);
   for (const collectionName of collections) {
       const collection = mongoose.connection.collections[collectionName];
-      await collection.deleteMany();
+      await collection.deleteMany({});
   }
 }
 
