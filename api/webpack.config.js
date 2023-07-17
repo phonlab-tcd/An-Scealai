59a5692f86e9b7b62f474958551e702681633070
Neimhin <norbinso@tcd.ie>
2023-07-17 14:42:07 +0000

    get frontend compiling

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -1,46 +1,40 @@
 const path = require('path');
 const isProduction = process.env.NODE_ENV == 'production';
 const config = {
-    target: 'node',
-    entry: './src/server.ts',
-    output: {
-        filename: 'server.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/i,
-                exclude: [
-                  path.resolve(__dirname, 'coverage/'),
-                  path.resolve(__dirname, 'bin/'),
-                  path.resolve(__dirname, 'node_modules/'),
-                ],
-                loader: 'ts-loader',
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
+  target: 'node',
+  entry: './src/server.ts',
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        exclude: [
+          path.resolve(__dirname, 'coverage/'),
+          path.resolve(__dirname, 'bin/'),
+          path.resolve(__dirname, 'node_modules/'),
         ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
-    },
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+      extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
+  },
 };
 
 module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-    } else {
-        config.mode = 'development';
-    }
-    return config;
+  if (isProduction) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+    config.watch = true;
+  }
+  return config;
 };
