570ab4b9b0841539c46da153e9858221e88114c2
Neimhin <norbinso@tcd.ie>
2023-07-17 13:12:42 +0000

    remove unneeded plugins from webpack config

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -1,36 +1,26 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
-
 const isProduction = process.env.NODE_ENV == 'production';
-
-
 const config = {
-	  target: 'node',
+    target: 'node',
     entry: './src/server.ts',
     output: {
+        filename: 'server.js',
         path: path.resolve(__dirname, 'dist'),
     },
     devServer: {
         open: true,
         host: 'localhost',
     },
-    plugins: [
-        // new HtmlWebpackPlugin({
-        //     template: 'index.html',
-        // }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-    ],
     module: {
         rules: [
             {
                 test: /\.(ts|tsx)$/i,
+                exclude: [
+                  path.resolve(__dirname, 'coverage/'),
+                  path.resolve(__dirname, 'bin/'),
+                  path.resolve(__dirname, 'node_modules/'),
+                ],
                 loader: 'ts-loader',
-                exclude: ['/node_modules/'],
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
@@ -49,10 +39,6 @@ const config = {
 module.exports = () => {
     if (isProduction) {
         config.mode = 'production';
-        
-        
-        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-        
     } else {
         config.mode = 'development';
     }
