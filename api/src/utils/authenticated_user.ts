87c5460374f24532661f845a2fa9a7d77df82329
Neimhin <nrobinso@tcd.ie>
2023-07-21 14:14:04 +0100

    throw API500Error is no authenticated user

--- api/src/utils/authenticated_user.ts
+++ api/src/utils/authenticated_user.ts
@@ -1,6 +1,7 @@
 import { Request } from "express";
+import { API500Error } from "./APIError";
 export default function authenticated_user(req: Request) {
   const user = (req as any).user;
-  if(!user) throw new Error("no authenticated user");
+  if(!user) throw new API500Error("no authenticated user");
   return user;
 }
