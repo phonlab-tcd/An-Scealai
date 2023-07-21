2649404274c26fc875153897f13b806798c726ba
Maddie Comtois <maddie.comtois@gmail.com>
2023-07-21 13:12:13 +0100

    remove span attributes

--- ngapp/src/app/student/dashboard/dashboard.component.ts
+++ ngapp/src/app/student/dashboard/dashboard.component.ts
@@ -397,9 +397,10 @@ export class DashboardComponent implements OnInit {
       return "";
     }
     return text
-      .replace(/\s*style="([^"])+"/g, "")
-      .replace(/\s*highlight-tag-type="([^"])+"/g, "")
-      .replace(/\s*highlight-tag="([^"])+"/g, "");
+      .replace(/\s*id="([^"])+"/g, "")
+      .replace(/\s*highlight-tag="([^"])+"/g, "")
+      .replace(/\s*left-edge=".*?"/g, '')
+      .replace(/\s*right-edge=".*?"/g, '');
   }
 
   /* Toggle upper menu buttons */
@@ -436,6 +437,7 @@ export class DashboardComponent implements OnInit {
 
   /* Download story in selected format */
   downloadStory() {
+    this.debounceSaveStory();
     this.dialogRef = this.dialog.open(BasicDialogComponent, {
       data: {
         title: this.ts.l.download,
