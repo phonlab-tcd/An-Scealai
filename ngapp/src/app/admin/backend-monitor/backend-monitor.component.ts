0378ac46a3661809faef9ded26ac694f92213fd2
Neimhin <nrobinso@tcd.ie>
2023-07-28 16:24:59 +0100

    spring clean

--- ngapp/src/app/admin/backend-monitor/backend-monitor.component.ts
+++ ngapp/src/app/admin/backend-monitor/backend-monitor.component.ts
@@ -374,7 +374,6 @@ async function main() {
   styleUrls: ['./backend-monitor.component.scss']
 })
 export class BackendMonitorComponent implements OnInit {
-
   constructor(
 		private http: HttpClient,
 		public sanitizer:DomSanitizer,
@@ -399,20 +398,5 @@ export class BackendMonitorComponent implements OnInit {
   ngOnInit(): void {
 		this.heartbeat();
 		main();
-    // The express monitor app needs to know which backend url we're using
-    // in order to set up a websocket to stream data about the server.
-    // 
-    // Here, we pass the backend url in a global variable on window object so that
-    // we can set up the websocket stream based on whatever backed url is in abairconfig.
-    window['abairconfig.baseurl'] = config.baseurl;
-    this.http.get(config.baseurl + 'status', {responseType: 'blob'}).subscribe(blob => {
-      this.expressMonitorObject = this.sanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(blob));
-    });
-  }
-
-  setIframeSize(iframe: HTMLIFrameElement): void {
-    iframe.style.height = '0'; // Reset the height to avoid scrollbars initially
-    iframe.style.height = iframe.contentWindow?.document.body.scrollHeight + 100 + 'px';
-    iframe.style.width = '100%';
   }
 }
